01c779b9cfd3d70b4bf4ed1c389abc4d879e51ed
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-07-19 11:40:04 +0200

    for meso

--- tools/data_organization.py
+++ tools/data_organization.py
@@ -5,4 +5,4 @@ def file_name_from_path(input_path, subject, f):
     full_path = os.path.join(input_path, subject, "scans", f, "resources", "NIFTI", "files")
     for file in os.listdir(full_path):
         if file.endswith(".nii"):
-            return file, os.path.join(full_path, file)
+            return file, full_path
