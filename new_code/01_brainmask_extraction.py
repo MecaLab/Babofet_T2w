c47977b51b40171ad37122d72a80dec6e2e6f789
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-07-23 11:35:08 +0200

    for meso

--- new_code/01_brainmask_extraction.py
+++ new_code/01_brainmask_extraction.py
@@ -36,7 +36,7 @@ echo "Running on: $SLURM_NODELIST"
 
 if __name__ == "__main__":
 
-    base_path = cfg.MESO_DATA_PATH
+    base_path = cfg.DATA_PATH
 
     subject_IDs = os.listdir(base_path)
     subject_processed_haste = list()
@@ -56,7 +56,8 @@ if __name__ == "__main__":
         for d in dir_list:
             d_lower = d.lower()
             if "haste" in d_lower:
-                haste_files.append(d)
+                # haste_files.append(d)
+                pass
             if "trufi" in d_lower:
                 truefisp_files.append(d)
 
@@ -109,6 +110,9 @@ if __name__ == "__main__":
                 bm_nifti_filename = s_nifti_filename[0] + "_brainmask.nii"
 
                 if not os.path.exists(os.path.join(bm_truefisp_subj_output_dir, bm_nifti_filename)):
+                    print(bm_truefisp_subj_output_dir)
+                    print(bm_nifti_filename)
+                    print(os.path.join(bm_truefisp_subj_output_dir, bm_nifti_filename))
 
                     write_slurm_file(input_path=nifti_full_path, output_path=bm_truefisp_subj_output_dir,
                                      input_file=nifti_filename, output_file=bm_nifti_filename)
@@ -119,6 +123,7 @@ if __name__ == "__main__":
                     # Might be long but to avoid multiple run on same GPU
                     time.sleep(35)
                     print("\t\tFinished TRUEFISP {}".format(bm_nifti_filename))
+                    exit()
                 else:
                     print("\t\tSkiped {}".format(bm_nifti_filename))
             print("\tEnding TRUEFISP {}".format(subject))
