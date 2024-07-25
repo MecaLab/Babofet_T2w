8b0c306cfb8dda6290216706bc7b72f3e2dcbcf9
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-07-25 09:46:12 +0200

    for meso

--- tools/assert_files.py
+++ tools/assert_files.py
@@ -56,6 +56,34 @@ def check_files(origin_path, destination_path):
         print("Everything OK for TRUEFISP")
 
 
+
+def check_size(origin_path, destination_path):
+    for subj in os.listdir(origin_path):
+
+        dir_list = os.listdir(os.path.join(origin_path, subj, "scans"))
+        haste_files = list()
+        truefisp_files = list()
+
+        for d in dir_list:
+            d_lower = d.lower()
+            if "trufi" in d_lower:
+                truefisp_files.append(d)
+            if "haste" in d_lower:
+                haste_files.append(d)
+
+        for f in haste_files:
+            nifti_filename, nifti_full_path = tdo.file_name_from_path(origin_path, subj, f)
+            brainmask_filename = nifti_filename.replace(".nii", "_brainmask.nii")
+
+            brainmask_full_path = os.path.join(destination_path, subj, "brainmask", brainmask_filename)
+            print(os.path.join(nifti_full_path, nifti_filename))
+            print(brainmask_full_path)
+            break
+        break
+
+
+
+
 if __name__ == "__main__":
     input_path = cfg.MESO_DATA_PATH
     dst_path = cfg.MESO_OUTPUT_PATH
