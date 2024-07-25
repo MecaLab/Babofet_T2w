a476bbac0501ce94b13efb0e2113987a9df48f60
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-07-25 09:53:28 +0200

    for meso

--- tools/assert_files.py
+++ tools/assert_files.py
@@ -58,6 +58,11 @@ def check_files(origin_path, destination_path):
 
 
 def check_size(origin_path, destination_path):
+    cpt_fail_haste = 0
+    cpt_fail_truefisp = 0
+    files_failed_haste = list()
+    files_failed_truefisp = list()
+
     for subj in os.listdir(origin_path):
 
         dir_list = os.listdir(os.path.join(origin_path, subj, "scans"))
@@ -80,9 +85,34 @@ def check_size(origin_path, destination_path):
             img_src = nb.load(os.path.join(nifti_full_path, nifti_filename))
             img_dst = nb.load(brainmask_full_path)
 
-            print(img_src.shape == img_dst.shape)
-            break
-        break
+            if img_src.shape != img_dst.shape:
+                files_failed_haste.append([img_src, img_dst])
+                cpt_fail_haste += 1
+
+        for f in truefisp_files:
+            nifti_filename, nifti_full_path = tdo.file_name_from_path(origin_path, subj, f)
+            brainmask_filename = nifti_filename.replace(".nii", "_brainmask.nii")
+
+            brainmask_full_path = os.path.join(destination_path, subj, "brainmask", brainmask_filename)
+
+            img_src = nb.load(os.path.join(nifti_full_path, nifti_filename))
+            img_dst = nb.load(brainmask_full_path)
+
+            if img_src.shape != img_dst.shape:
+                files_failed_truefisp.append([img_src, img_dst])
+                cpt_fail_truefisp += 1
+
+    if cpt_fail_haste != 0:
+        print("Number of failed HASTE files: ", cpt_fail_haste)
+        print(files_failed_haste)
+    else:
+        print("Everything OK for HASTE")
+
+    if cpt_fail_truefisp != 0:
+        print("Number of failed TRUEFISP files: ", cpt_fail_truefisp)
+        print(files_failed_truefisp)
+    else:
+        print("Everything OK for TRUEFISP")
 
 
 if __name__ == "__main__":
