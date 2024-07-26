f2a3671e5fa4da2bb168540f3391efed511940b2
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-07-26 13:40:33 +0200

    for meso

--- new_code/04_3DHR_image_reconstruction.py
+++ new_code/04_3DHR_image_reconstruction.py
@@ -80,6 +80,8 @@ if __name__ == '__main__':
             bm_img = list()
             for f in haste_files:
                 niftif_filename = os.path.join(denoised_subj_output_dir, f)
+                print(niftif_filename)
+                exit()
                 bm_nifti_filename = s_nifti_filename[0] + "_brainmask_resampled.nii"
                 bm_output_file = os.path.join(bm_haste_subj_output_dir, bm_nifti_filename)
 
