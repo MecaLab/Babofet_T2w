6c18ac2db6d18eef8be2221c4e2c59dffb0eb4ec
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-05 11:30:18 +0200

    for meso

--- new_code/04_3DHR_image_reconstruction.py
+++ new_code/04_3DHR_image_reconstruction.py
@@ -28,9 +28,9 @@ MAIN_PATH="{main_path}"
 INPUT_PATH="${{MAIN_PATH}}/denoising"
 MASK_PATH="${{MAIN_PATH}}/brainmask"
 
-OUTPUT_PATH="${{MAIN_PATH}}/haste"
+OUTPUT_PATH="${{MAIN_PATH}}/haste/reconstruction_ebner"
 MOTION_CORRECTION="${{OUTPUT_PATH}}/motion_correction"
-OUTPUT_FILE="${{OUTPUT_PATH}}/reconstruction_ebner/{output_file}"
+OUTPUT_FILE="{output_file}"
 
 echo $OUTPUT_PATH
 echo $OUTPUT_FILE
