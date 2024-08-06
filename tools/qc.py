e3d16bb504190e40e82a80e2ae210d1a4dbe238f
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-06 16:19:02 +0200

    for meso

--- tools/qc.py
+++ tools/qc.py
@@ -43,7 +43,7 @@ def qc_brainmask(path_anat_vol, path_brainmask_vol, file_figure_out):
         plt.figure(figsize=(6, 6))
 
         plt.title("Superposition de la coupe de cerveau et du brainmask")
-        plt.imshow(brain_data[:, :, slice_index], cmap='gray')
+        plt.imshow(brain_data[slice_index, :, :], cmap='gray')
         plt.imshow(brain_mask_data[:, :, slice_index], cmap='hot',
                    alpha=0.3)  # Ajuster l'alpha pour la transparence
         plt.axis('off')
