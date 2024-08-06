6f599e04a9e5f3fb4bf7bc6894cebbafb0fe2307
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-06 10:27:37 +0200

    for meso

--- nesvor_module/02_brainmask_extraction.py
+++ nesvor_module/02_brainmask_extraction.py
@@ -69,7 +69,10 @@ if __name__ == "__main__":
                 os.mkdir(bm_haste_subj_output_dir)
 
             for f in haste_files:
-                print(f) 
+                nifti_filename, nifti_full_path = f, os.path.join(subj_output_dir, f)
+                print(nifti_filename)
+                print(nifti_full_path)
+                exit()
                 # nifti_filename, nifti_full_path = tdo.file_name_from_path(base_path, subject, f)
                 s_nifti_filename = nifti_filename.split(".")
                 bm_nifti_filename = s_nifti_filename[0] + "_brainmask.nii"
