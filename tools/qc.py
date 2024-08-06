d04b977c287155bfa0e5503f2ef1b517125079cf
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-06 16:18:33 +0200

    for meso

--- tools/qc.py
+++ tools/qc.py
@@ -23,47 +23,44 @@ def qc_brainmask(path_anat_vol, path_brainmask_vol, file_figure_out):
     if not os.path.exists(path_brainmask_vol):
         print(path_brainmask_vol + " not found, skip!")
     else:
-        if os.path.exists(file_figure_out):
-            print(file_figure_out + " already exists, skip!")
-        else:
 
-            print(path_brainmask_vol)
-            print(path_anat_vol)
+        print(path_brainmask_vol)
+        print(path_anat_vol)
 
-            anat_img = nib.load(path_anat_vol)
-            bm_img = nib.load(path_brainmask_vol)
-            """
-            dims_anat = anat_img.shape
-            dim_bm = bm_img.shape
-            """
+        anat_img = nib.load(path_anat_vol)
+        bm_img = nib.load(path_brainmask_vol)
+        """
+        dims_anat = anat_img.shape
+        dim_bm = bm_img.shape
+        """
 
-            brain_data = anat_img.get_fdata()
-            brain_mask_data = bm_img.get_fdata()
+        brain_data = anat_img.get_fdata()
+        brain_mask_data = bm_img.get_fdata()
 
-            slice_index = brain_data.shape[2] // 2
+        slice_index = brain_data.shape[0] // 2
 
-            # Afficher la coupe du cerveau
-            plt.figure(figsize=(6, 6))
+        # Afficher la coupe du cerveau
+        plt.figure(figsize=(6, 6))
 
-            plt.title("Superposition de la coupe de cerveau et du brainmask")
-            plt.imshow(brain_data[:, :, slice_index], cmap='gray')
-            plt.imshow(brain_mask_data[:, :, slice_index], cmap='hot',
-                       alpha=0.3)  # Ajuster l'alpha pour la transparence
-            plt.axis('off')
+        plt.title("Superposition de la coupe de cerveau et du brainmask")
+        plt.imshow(brain_data[:, :, slice_index], cmap='gray')
+        plt.imshow(brain_mask_data[:, :, slice_index], cmap='hot',
+                   alpha=0.3)  # Ajuster l'alpha pour la transparence
+        plt.axis('off')
 
-            plt.savefig(file_figure_out)
+        plt.savefig(file_figure_out)
 
-            """nisnap.plot_segment(
-                path_brainmask_vol,
-                bg=path_anat_vol,
-                axes="x",
-                figsize=figsize,
-                opacity=50,
-                samebox=True,
-                # labels=[1],
-                # contours=True,
-                savefig=file_figure_out,
-            )"""
+        """nisnap.plot_segment(
+            path_brainmask_vol,
+            bg=path_anat_vol,
+            axes="x",
+            figsize=figsize,
+            opacity=50,
+            samebox=True,
+            # labels=[1],
+            # contours=True,
+            savefig=file_figure_out,
+        )"""
 
 
 def qc_recontructed_3DHRvolume(path_anat_vol, file_figure_out):
