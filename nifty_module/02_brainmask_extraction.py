dcdbc9cee2fed5f42268b703a3a769cae4f4c9bb
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-07 15:52:02 +0200

    for meso

--- nifty_module/02_brainmask_extraction.py
+++ nifty_module/02_brainmask_extraction.py
@@ -62,5 +62,7 @@ if __name__ == "__main__":
                 nifti_filename, nifti_full_path = f, os.path.join(subj_output_dir, "denoising")
 
                 s_nifti_filename = nifti_filename.split(".")
+                print(os.path.join(bm_haste_subj_output_dir, s_nifti_filename[0]))
                 if not os.path.exists(os.path.join(bm_haste_subj_output_dir, s_nifti_filename[0])):
                     print("not exist")
+                exit()
