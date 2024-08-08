8a6cc3df96be723ecf4c4ba801498fe1660db242
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-08 14:15:21 +0200

    for meso

--- tools/qc.py
+++ tools/qc.py
@@ -34,10 +34,9 @@ def qc_brainmask(path_anat_vol, path_brainmask_vol, file_figure_out, debug=False
             if debug:
                 print(f"ANAT shape after Transpose: {brain_data.shape}")
 
-        print(brain_mask_data.shape)
-
         brain_mask_data = np.squeeze(brain_mask_data)
-        brain_mask_data = np.transpose(brain_mask_data, (2, 0, 1))
+        if brain_mask_data.shape != brain_data.shape:
+            brain_mask_data = np.transpose(brain_mask_data, (2, 0, 1))
 
         brain_shape = brain_data.shape
         bm_shape = brain_mask_data.shape
