4c21a685f4ae7c73088814a89744a7d9956d6ddb
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-09 11:26:17 +0200

    for meso

--- tools/qc.py
+++ tools/qc.py
@@ -74,14 +74,11 @@ def qc_brainmask(path_anat_vol, path_brainmask_vol, file_figure_out, debug=False
             data[brain_mask_data == 1] = 2
             fake_mask = nib.Nifti1Image(
                 data,
-                affine=bm_img.affine,
+                affine=bm_img_reoriented.affine,
                 header=bm_img.header,
             )
-            if not np.array_equal(anat_img.affine, fake_mask.affine):
-                fake_mask = nib.Nifti1Image(bm_img_reoriented.get_fdata(), anat_img_reoriented.affine,
-                                                    fake_mask.header)
-
             nib.save(fake_mask, tmpfile_mask.name)
+            
             nisnap.plot_segment(
                 tmpfile_mask.name,
                 bg=path_anat_vol,
