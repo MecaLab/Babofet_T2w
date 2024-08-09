2140793fde66873b808dba34ce5afd7235083d5c
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-09 11:14:48 +0200

    for meso

--- tools/qc_brainmask.py
+++ tools/qc_brainmask.py
@@ -39,7 +39,8 @@ if __name__ == "__main__":
                 qc.qc_brainmask(
                     os.path.join(dir_denoised, f),
                     os.path.join(dir_brainmask, filename[0], bm_nifti_filename),
-                    file_figure_out
+                    file_figure_out,
+                    debug=True
                 )
                 print(f"End {f} for {subject}")
         elif MODE == "nesvor":
