fd3fef83d55a0f93d23c3fb0054979363284b569
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-08-09 11:08:42 +0200

    for meso

--- tools/qc.py
+++ tools/qc.py
@@ -18,11 +18,11 @@ def qc_brainmask(path_anat_vol, path_brainmask_vol, file_figure_out, debug=False
     :param file_figure_out: path of the output file
     :return:
     """
+
+    """elif os.path.exists(file_figure_out):
+        print(f"{file_figure_out} already exist, skip!")"""
     if not os.path.exists(path_brainmask_vol):
         print(path_brainmask_vol + " not found, skip!")
-    elif os.path.exists(file_figure_out):
-        pass
-        # print(f"{file_figure_out} already exist, skip!")
     else:
         figsize = {'z': (10, 7)}
         anat_img = nib.load(path_anat_vol)
