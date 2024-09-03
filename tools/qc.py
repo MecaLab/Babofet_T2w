67cee75284bddc9f96012ca82f254f3af1236e49
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-09-03 10:51:56 +0200

    for meso

--- tools/qc.py
+++ tools/qc.py
@@ -75,7 +75,7 @@ def qc_brainmask(path_anat_vol, path_brainmask_vol, file_figure_out, debug=False
             data = np.ones_like(brain_mask_data)
             data[brain_mask_data == 1] = 2
 
-            print(brain_mask_data == 1)
+            print(np.sum(brain_mask_data == 1))
 
             fake_mask = nib.Nifti1Image(
                 data,
