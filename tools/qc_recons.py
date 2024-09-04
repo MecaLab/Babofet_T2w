fa2f4804e64376ece15ddfe9eb5b62f9a85299dd
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-09-04 14:38:03 +0200

    for meso

--- tools/qc_recons.py
+++ tools/qc_recons.py
@@ -8,12 +8,15 @@ import configuration as cfg
 if __name__ == "__main__":
     MODE = "niftymic"  # "niftymic" | "nesvor"
     dir_snapshots = "snapshots"
+    mid_dir_snapshots = os.path.join(dir_snapshots, MODE, "recons")
+
+    if not os.path.exists(mid_dir_snapshots):
+        os.mkdir(mid_dir_snapshots)
 
     subject = "sub-Aziza_ses-04"
 
     path_recons = os.path.join(cfg.MESO_OUTPUT_PATH, subject, "haste", "reconstruction_niftymic")
 
-    for file in os.listdir(path_recons):
-        print(file)
+    dir_out = os.path.join
 
 
