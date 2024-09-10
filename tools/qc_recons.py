d2f2bb7738f89dece8fd117bb626262f0d9bda36
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-09-10 10:05:17 +0200

    for meso

--- tools/qc_recons.py
+++ tools/qc_recons.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     dir_snapshots = "snapshots"
 
     mid_dir_snapshots = os.path.join(dir_snapshots, "recons")
+
     if not os.path.exists(mid_dir_snapshots):
         os.mkdir(mid_dir_snapshots)
 
@@ -17,27 +18,36 @@ if __name__ == "__main__":
     if not os.path.exists(mid_dir_snapshots):
         os.mkdir(mid_dir_snapshots)
 
-    subject = "sub-Aziza_ses-04"
+    base_path = cfg.MESO_OUTPUT_PATH
 
-    path_recons = os.path.join(cfg.MESO_OUTPUT_PATH, subject, "haste", "reconstruction_niftymic")
+    subject_IDs = os.listdir(base_path)
 
-    dir_out = os.path.join(mid_dir_snapshots, subject)
-    if not os.path.exists(dir_out):
-        os.mkdir(dir_out)
+    for subject in subject_IDs:
+        if MODE == "niftymic":
+            if "Aziza" not in subject:
+                continue
 
-    anat_filename = subject + "_haste_3DHR.nii.gz"
-    bm_filename = subject + "_haste_3DHR_mask.nii.gz"
+            print(f"Snapshot for {subject}")
+            path_recons = os.path.join(cfg.MESO_OUTPUT_PATH, subject, "haste", "reconstruction_niftymic")
 
-    anat_path = os.path.join(path_recons, anat_filename)
-    bm_path = os.path.join(path_recons, bm_filename)
+            dir_out = os.path.join(mid_dir_snapshots, subject)
+            if not os.path.exists(dir_out):
+                os.mkdir(dir_out)
 
-    #file_figure_out = os.path.join(dir_out, subject + "_recons.png")
-    file_figure_out = os.path.join(dir_out, "tmp.png")
+            anat_filename = subject + "_haste_3DHR.nii.gz"
+            bm_filename = subject + "_haste_3DHR_mask.nii.gz"
 
-    qc.qc_recontructed_3DHRvolume(
-        path_anat_vol=anat_path,
-        path_brainmask_vol=bm_path,
-        file_figure_out=file_figure_out
-    )
+            anat_path = os.path.join(path_recons, anat_filename)
+            bm_path = os.path.join(path_recons, bm_filename)
+
+            file_figure_out = os.path.join(dir_out, subject + "_recons.png")
+            qc.qc_recontructed_3DHRvolume(
+                path_anat_vol=anat_path,
+                path_brainmask_vol=bm_path,
+                file_figure_out=file_figure_out
+            )
+        elif MODE == "nesvor":
+            pass
+
 
 
