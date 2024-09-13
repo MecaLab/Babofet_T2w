88e4d73041e47620fc8d580d241062292dbe2cdb
Baptiste LEROUX <leroux.b@intlocal.univ-amu.fr>
2024-09-13 10:15:25 +0200

    for meso

--- tools/extract_metadata.py
+++ tools/extract_metadata.py
@@ -15,7 +15,14 @@ def get_field(dicom, field):
 
 def get_dicom_directory_metadata(directory, fields, path_df):
     all_files = glob.iglob(os.path.join(directory, "**"), recursive=True)
-    print(all_files)
+    only_files = (f for f in all_files if os.path.isfile(f))
+
+    print(only_files)
+    # remove .info dicom files if any
+    only_dcm_files = (f for f in only_files if ".info" not in f)
+    # extract metadata per file and concatenate them at the directory level
+
+    # dfs = (get_dicom_file_metadata(f, fields) for f in only_dcm_files)
 
 
 if __name__ == "__main__":
