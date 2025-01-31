9d66b93276d381aa880af5fea487ce9c4883a7d1
leroux.b <baptiste.leroux@univ-amu.fr>
2025-01-31 14:08:25 +0100

    test manual mask

--- nifty_module/05_image_reconstruction.py
+++ nifty_module/05_image_reconstruction.py
@@ -114,6 +114,8 @@ if __name__ == "__main__":
                 anat_path_subj_path = os.path.join(denoised_subj_output_dir, f)
                 # bm_path_subj_path = os.path.join(bm_haste_subj_output_dir, filename[0], bm_nifti_filename) # for brainmask_niftymic folder
                 bm_path_subj_path = os.path.join(bm_haste_subj_output_dir, bm_nifti_filename)
+                
+                print(os.path.exists(bm_path_subj_path), bm_path_subj_path)
 
                 if os.path.exists(anat_path_subj_path) and os.path.exists(bm_path_subj_path):
                     anat_img.append(f)
