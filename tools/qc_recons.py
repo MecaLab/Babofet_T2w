8a04d75b585331a794515cda37b677635bd62316
leroux.b <baptiste.leroux@univ-amu.fr>
2025-02-13 10:38:14 +0100

    for meso

--- tools/qc_recons.py
+++ tools/qc_recons.py
@@ -164,8 +164,12 @@ def qc_rejected_slices(subj_path, subj, mode):
             bm_data = (bm_data == 1).astype(int)
 
             print(bm_data.shape)
-            exit()
+            if len(bm_data.shape) == 4:
+                bm_data = np.squeeze(bm_data)
 
+            print(bm_data.shape)
+            exit()
+            
             n_slices = img_data.shape[2]
             n_cols = 5
             n_rows = (n_slices + n_cols - 1) // n_cols
