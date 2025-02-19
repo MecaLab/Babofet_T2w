a87df14bf704cedd1b783947f73f81718d3b40c4
leroux.b <baptiste.leroux@univ-amu.fr>
2025-02-19 10:37:42 +0100

    for niolon

--- tools/fermeture.py
+++ tools/fermeture.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    dims = subprocess.check_output(f"fslval {input_file} dim3")
+    dims = subprocess.check_output(f"fslval {input_file} dim3", shell=True)
     print(dims)
