af84ac5a0cbaea34c028f4ca00d50286872b3859
leroux.b <baptiste.leroux@univ-amu.fr>
2025-02-19 10:36:09 +0100

    for niolon

---
+++ tools/fermeture.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+import sys
+
+if __name__ == "__main__":
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    dims = subprocess.check_output(f"fslval {input_file} dim3", shell=True)
+    print(dims)
