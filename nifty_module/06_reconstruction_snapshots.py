46e87608f7318414f738639e40a8a6808a186a83
leroux.b <baptiste.leroux@univ-amu.fr>
2025-02-25 14:20:11 +0100

    for meso

--- nifty_module/06_reconstruction_snapshots.py
+++ nifty_module/06_reconstruction_snapshots.py
@@ -6,7 +6,6 @@ from tools import qc_recons
 
 if __name__ == "__main__":
 
-    """
     subject = "Fabienne"
     base_path = os.path.join(cfg.DATA_PATH, subject)
     modes = ["manual", "nifty"]
@@ -19,17 +18,17 @@ if __name__ == "__main__":
         datas[session] = {}
         for mode in modes:
             datas[session][mode] = {}
-            datas[session][mode]["anat"] = os.path.join(base_path, session, f"{mode}_brainmask", f"sub-{subject}_ses-{session[3:]}_haste_3DHR_{mode}_bm_pipeline.nii.gz")
+            datas[session][mode]["anat"] = os.path.join(base_path, session, f"{mode}_brainmask", f"sub-{subject}_ses-{session[3:]}_haste_3DHR_{mode}_bm_T-1_pipeline.nii.gz")
 
-    qc_recons.qc_plot_table_recons(datas)"""
+    qc_recons.qc_plot_table_recons(datas)
 
-    subject = "Fabienne"
+    """subject = "Fabienne"
     modes = ["manual"]
     base_path = os.path.join(cfg.DATA_PATH, subject)
 
     for mode in modes:
         qc_recons.qc_recons_bis(base_path, subject, mode)
-
+    """
 
     """base_path = cfg.MESO_OUTPUT_PATH
     model = "niftymic"  # niftymic or nesvor
