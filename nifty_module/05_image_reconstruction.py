f76ec41954ce3caa91696b7003c477aa5e2d0f8c
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-05 15:57:06 +0100

    for meso

--- nifty_module/05_image_reconstruction.py
+++ nifty_module/05_image_reconstruction.py
@@ -55,8 +55,7 @@ singularity exec \\
         --filenames-masks {mask_stacks} \\
         --output /output/$OUTPUT_FILE \\
         --isotropic-resolution 0.5 \\
-        --threshold-first 0.4 \\
-        --threshold 0.6 \\
+        --two-step-cycle 4 \\
         
 ./mv_recons.sh {subj} {mode_bm}
 """
@@ -70,7 +69,7 @@ singularity exec \\
 """
 Parametres:
 - two-step-cycle: Changer le nombre de cycle (défault 3)
-- threshold(-first): est utilisé pour rejeter les slices (NCC). -first pour le premier cycle, rien pour le cycle au milieu (moyenne des 2?)
+- threshold(-first): est utilisé pour rejeter les slices (NCC). -first pour le premier cycle, rien pour le cycle au milieu (moyenne des 2)
 """
 
 
@@ -153,7 +152,7 @@ if __name__ == "__main__":
             if not os.path.exists(motion_subfolder):
                 os.mkdir(motion_subfolder)
 
-            recons_haste_subj_output = subject + f"_haste_3DHR_{mask_model}_bm_T46_pipeline.nii.gz"
+            recons_haste_subj_output = subject + f"_haste_3DHR_{mask_model}_bm_4C_pipeline.nii.gz"
 
             write_slurm_file_nifty(
                 subj=subject,
