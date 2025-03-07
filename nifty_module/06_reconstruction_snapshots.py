ca5fd74174e5a53aac3ffe36da92fd8dd59d439e
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-07 11:07:04 +0100

    for meso

--- nifty_module/06_reconstruction_snapshots.py
+++ nifty_module/06_reconstruction_snapshots.py
@@ -22,7 +22,7 @@ def plot_intensity_profile(data, slice_index, axis=0, label=''):
     elif axis == 1:
         profile = data[:, slice_index, :].mean(axis=1)
     elif axis == 2:
-        profile = data[:, :, slice_index]
+        profile = data[:, :, slice_index].mean(axis=0)
 
     plt.plot(profile, label=label)
     plt.xlabel('Position')
@@ -59,41 +59,41 @@ if __name__ == "__main__":
     """
 
     volume_ref = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/sub-Fabienne_ses-01_haste_3DHR_manual_bm_pipeline.nii.gz")
-    volume_1 = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/exp_param/sub-Fabienne_ses-01_haste_3DHR_manual_bm_T-1_pipeline.nii.gz")
-    volume_2 = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/exp_param/sub-Fabienne_ses-01_haste_3DHR_manual_bm_T13_pipeline.nii.gz")
-    volume_3 = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/exp_param/sub-Fabienne_ses-01_haste_3DHR_manual_bm_T46_pipeline.nii.gz")
+    volume_1 = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/exp_param/sub-Fabienne_ses-01_haste_3DHR_manual_bm_4C_pipeline.nii.gz")
+    """volume_2 = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/exp_param/sub-Fabienne_ses-01_haste_3DHR_manual_bm_T13_pipeline.nii.gz")
+    volume_3 = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/exp_param/sub-Fabienne_ses-01_haste_3DHR_manual_bm_T46_pipeline.nii.gz")"""
 
     volume_ref_data = volume_ref.get_fdata()
     volume_1_data = volume_1.get_fdata()
-    volume_2_data = volume_2.get_fdata()
-    volume_3_data = volume_3.get_fdata()
+    """volume_2_data = volume_2.get_fdata()
+    volume_3_data = volume_3.get_fdata()"""
 
     origin_output_path = "/scratch/lbaptiste/Babofet_T2w/snapshots"
 
     plt.figure(figsize=(12, 6))
-    plot_histo(volume_1_data, "Threshold -1")
-    plot_histo(volume_2_data, "Threshold 13")
-    plot_histo(volume_3_data, "Threshold 46")
-    plot_histo(volume_ref_data, "Default threshold")
-    plt.savefig(os.path.join(origin_output_path, "histo.png"))
+    plot_histo(volume_1_data, "Two step cycle 4")
+    """plot_histo(volume_2_data, "Threshold 13")
+    plot_histo(volume_3_data, "Threshold 46")"""
+    plot_histo(volume_ref_data, "Default two step cycle")
+    plt.savefig(os.path.join(origin_output_path, "histo_tsc.png"))
     plt.close()
 
     plt.figure(figsize=(12, 6))
-    plot_intensity_profile(volume_1_data, 50, axis=2, label="Threshold -1")
-    plot_intensity_profile(volume_2_data, 50, axis=2, label="Threshold 13")
-    plot_intensity_profile(volume_3_data, 50, axis=2, label="Threshold 46")
-    plot_intensity_profile(volume_ref_data, 50, axis=2, label="Default threshold")
-    plt.savefig(os.path.join(origin_output_path, "intensity.png"))
+    plot_intensity_profile(volume_1_data, 50, axis=2, label="Two step cycle 4")
+    """plot_intensity_profile(volume_2_data, 50, axis=2, label="Threshold 13")
+    plot_intensity_profile(volume_3_data, 50, axis=2, label="Threshold 46")"""
+    plot_intensity_profile(volume_ref_data, 50, axis=2, label="Default two step cycle")
+    plt.savefig(os.path.join(origin_output_path, "intensity_tsc.png"))
     plt.close()
 
     mean1, std1 = np.mean(volume_ref_data), np.std(volume_ref_data)
     mean2, std2 = np.mean(volume_1_data), np.std(volume_1_data)
-    mean3, std3 = np.mean(volume_2_data), np.std(volume_2_data)
-    mean4, std4 = np.mean(volume_3_data), np.std(volume_3_data)
+    """mean3, std3 = np.mean(volume_2_data), np.std(volume_2_data)
+    mean4, std4 = np.mean(volume_3_data), np.std(volume_3_data)"""
     print(f'Volume 1 (ref) - Moyenne: {mean1} | Écart-type: {std1}')
     print(f'Volume 2 - Moyenne: {mean2} | Écart-type: {std2}')
-    print(f'Volume 3 - Moyenne: {mean3} | Écart-type: {std3}')
-    print(f'Volume 4 - Moyenne: {mean4} | Écart-type: {std4}')
+    """print(f'Volume 3 - Moyenne: {mean3} | Écart-type: {std3}')
+    print(f'Volume 4 - Moyenne: {mean4} | Écart-type: {std4}')"""
 
 
     """
