4e41a07c6eb1c147e53d9d26f9caff49866d2827
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-07 10:57:54 +0100

    for meso

--- nifty_module/06_reconstruction_snapshots.py
+++ nifty_module/06_reconstruction_snapshots.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
             datas[session][mode]["anat"] = os.path.join(base_path, session, f"{mode}_brainmask", f"sub-{subject}_ses-{session[3:]}_haste_3DHR_{mode}_bm_pipeline.nii.gz")
 
     # qc_recons.qc_plot_table_recons(datas, name)
-    """
+    
 
     # 1 snapshot per reconstruction
     modes = ["manual"]
@@ -56,8 +56,8 @@ if __name__ == "__main__":
 
     for mode in modes:
         qc_recons.qc_recons_bis(base_path, subject, mode)
-
     """
+
     volume_ref = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/sub-Fabienne_ses-01_haste_3DHR_manual_bm_pipeline.nii.gz")
     volume_1 = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/exp_param/sub-Fabienne_ses-01_haste_3DHR_manual_bm_T-1_pipeline.nii.gz")
     volume_2 = nib.load("/scratch/lbaptiste/data/recons_folder/Fabienne/ses01/manual_brainmask/exp_param/sub-Fabienne_ses-01_haste_3DHR_manual_bm_T13_pipeline.nii.gz")
@@ -68,12 +68,14 @@ if __name__ == "__main__":
     volume_2_data = volume_2.get_fdata()
     volume_3_data = volume_3.get_fdata()
 
+    origin_output_path = "/scratch/lbaptiste/Babofet_T2w/snapshots"
+
     plt.figure(figsize=(12, 6))
     plot_histo(volume_1_data, "Threshold -1")
     plot_histo(volume_2_data, "Threshold 13")
     plot_histo(volume_3_data, "Threshold 46")
     plot_histo(volume_ref_data, "Default threshold")
-    plt.savefig("tmp.png")
+    plt.savefig(os.path.join(origin_output_path, "histo.png"))
     plt.close()
 
     plt.figure(figsize=(12, 6))
@@ -81,7 +83,7 @@ if __name__ == "__main__":
     plot_intensity_profile(volume_2_data, 50, axis=2, label="Threshold 13")
     plot_intensity_profile(volume_3_data, 50, axis=2, label="Threshold 46")
     plot_intensity_profile(volume_ref_data, 50, axis=2, label="Default threshold")
-    plt.savefig("tmp2.png")
+    plt.savefig(os.path.join(origin_output_path, "intensity.png"))
     plt.close()
 
     mean1, std1 = np.mean(volume_1_data), np.std(volume_1_data)
@@ -89,6 +91,8 @@ if __name__ == "__main__":
     print(f'Volume 1 - Moyenne: {mean1} | Écart-type: {std1}')
     print(f'Volume 2 - Moyenne: {mean2} | Écart-type: {std2}')
 
+
+    """
     base_path = cfg.MESO_OUTPUT_PATH
     model = "niftymic"  # niftymic or nesvor
     mode = "manual"  # manual bm or nifty bm
