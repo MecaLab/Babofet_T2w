c8256c6b5b06c621ce04b76850acf7d74dad0f06
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-12 15:27:02 +0100

    for meso

--- nifty_module/06_reconstruction_snapshots.py
+++ nifty_module/06_reconstruction_snapshots.py
@@ -35,13 +35,15 @@ if __name__ == "__main__":
             for mode in modes:
                 print(f"Session {session} - Mode {mode}")
                 subj_path = os.path.join(base_path, session)  # ../data/recons_folder/subj/session
+                print(subj_path)
+                exit()
                 subj_session = f"sub-{subject}_ses-{session[3:]}"
 
                 # Plot the anat image with the BM using the rejected slices file
                 qc_recons.qc_rejected_slices(subj_path, subject, subj_session, mode, exp_param_folder=exp_param_folder, param=param, name=name)
 
                 # Plot 1 snapshot per reconstruction
-                qc_recons.qc_recons_bis(base_path, subject, mode, exp_param_folder=exp_param_folder, param=param, name=name)
+                qc_recons.qc_recons_bis(subj_path, subject, mode, exp_param_folder=exp_param_folder, param=param, name=name)
 
                 qc_recons.qc_intensity(subj_path, subject, mode, subj_session, param="T")
 
