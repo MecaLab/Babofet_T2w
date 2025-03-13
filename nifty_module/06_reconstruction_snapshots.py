cd16fcb5f89cce7f57664b69300a02a056fb95c4
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-13 09:56:05 +0100

    for meso

--- nifty_module/06_reconstruction_snapshots.py
+++ nifty_module/06_reconstruction_snapshots.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
                 subj_session = f"sub-{subject}_ses-{session[3:]}"
 
                 # Plot the anat image with the BM using the rejected slices file
-                qc_recons.qc_rejected_slices(subj_path, subject, subj_session, mode, exp_param_folder=exp_param_folder, param=param, name=name)
+                # qc_recons.qc_rejected_slices(subj_path, subject, subj_session, mode, exp_param_folder=exp_param_folder, param=param, name=name)
 
                 # Plot 1 snapshot per reconstruction
                 # qc_recons.qc_recons_bis(subj_path, subject, mode, exp_param_folder=exp_param_folder, param=param, name=name)
