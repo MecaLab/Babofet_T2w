e868122f52f11104bb0123056420d1ab2529edf2
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-13 11:27:09 +0100

    for meso

--- nifty_module/06_reconstruction_snapshots.py
+++ nifty_module/06_reconstruction_snapshots.py
@@ -44,7 +44,8 @@ if __name__ == "__main__":
     fig, axes = plt.subplots(len(indices), len(vols), figsize=(15, 10))
 
     for i, idx in enumerate(indices):
-        axes[i, 0].text(0.5, 0.5, f"Slice {idx}", ha='left', va='center', fontsize=12)
+        axes[i, 0].text(-20, 0.5, f"Slice {idx}", transform=axes[i, 0].transAxes, va='center', ha='right', fontsize=12,
+                        fontweight='bold')
         for j, (param, vol) in enumerate(vols.items()):
             ax = axes[i, j]
             axes[0, j].set_title(f"{param}", fontsize=12, fontweight='bold')
