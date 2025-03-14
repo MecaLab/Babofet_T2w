75a4ff761b10a678515354f9aaa633787a8d0c30
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-14 13:17:02 +0100

    for meso

--- nifty_module/05_image_reconstruction.py
+++ nifty_module/05_image_reconstruction.py
@@ -56,8 +56,8 @@ singularity exec \\
         --output /output/$OUTPUT_FILE \\
         --isotropic-resolution 0.5 \\
         --bias-field-correction 1 \\
-        --threshold-first 0.4 \\
-        --threshold 0.6 \\
+        --threshold-first -1 \\
+        --threshold -1 \\
         
 ./mv_recons.sh {subj} {mode_bm}
 """
@@ -155,7 +155,7 @@ if __name__ == "__main__":
             if not os.path.exists(motion_subfolder):
                 os.mkdir(motion_subfolder)
 
-            recons_haste_subj_output = subject + f"_haste_3DHR_{mask_model}_bm_B1_T46_pipeline.nii.gz"
+            recons_haste_subj_output = subject + f"_haste_3DHR_{mask_model}_bm_B1_T-1_pipeline.nii.gz"
 
             write_slurm_file_nifty(
                 subj=subject,
