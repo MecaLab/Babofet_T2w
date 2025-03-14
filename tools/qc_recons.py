34f427d6f1addc87456c1570a7aaf5bab328abeb
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-14 15:09:49 +0100

    for meso

--- tools/qc_recons.py
+++ tools/qc_recons.py
@@ -395,7 +395,7 @@ def qc_plot_table_params(subj_path, mode, subject, subj_session):
         if file.endswith("pipeline.nii.gz"):
             print(file)
             vol = nib.load(os.path.join(nib_path, "exp_param", file)).get_fdata()
-            print(file.split("_"))
+            print(file.split("bm_"))
             param = file.split("_")[-2]
             print(param)
             vols[param] = vol
