24e800ad5c890885fe2ad4905112646bd9160545
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-19 13:27:48 +0100

    for meso'

--- nifty_module/06_reconstruction_snapshots.py
+++ nifty_module/06_reconstruction_snapshots.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
     modes = ["manual"]
     datas = {}
 
-    exp_list = [False, True, True, True, True, True, True, True]
-    params = [None, "T-1", "T13", "T46", "B1", "B1_T-1", "B1_T13", "B1_T46"]
-    names = ["default-param", "threshold_-1", "threshold_0.1_0.3", "threshold_0.4_0.6", "bias-field-correction", "bias-field-correction_threshold_-1", "bias-field-correction_threshold_0.1_0.3", "bias-field-correction_threshold_0.4_0.6"]
+    exp_list = [False, True, True, True, ] # True, True, True, True]
+    params = [None, "T-1", "T13", "T46", ] # "B1", "B1_T-1", "B1_T13", "B1_T46"]
+    names = ["default-param", "threshold_-1", "threshold_0.1_0.3", "threshold_0.4_0.6", ] # "bias-field-correction", "bias-field-correction_threshold_-1", "bias-field-correction_threshold_0.1_0.3", "bias-field-correction_threshold_0.4_0.6"]
 
     for i in range(len(exp_list)):
         exp_param_folder = exp_list[i]
