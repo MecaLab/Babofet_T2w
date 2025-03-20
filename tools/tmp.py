7acfef643eb2e26c5c6bda705ed3fc6565109540
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-20 16:29:29 +0100

    for meso

--- tools/tmp.py
+++ tools/tmp.py
@@ -10,8 +10,8 @@ session = "01"
 
 vol_1_path = os.path.join(base_path, f"ses{session}/manual_brainmask", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_pipeline.nii.gz")
 mask_1_path = os.path.join(base_path, f"ses{session}/manual_brainmask", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_pipeline_mask.nii.gz")
-vol_2_path = os.path.join(base_path, f"ses{session}/manual_brainmask/exp_param", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_T-1_pipeline.nii.gz")
-mask_2_path = os.path.join(base_path, f"ses{session}/manual_brainmask/exp_param", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_T-1_pipeline_mask.nii.gz")
+vol_2_path = os.path.join(base_path, f"ses{session}/manual_brainmask/exp_param", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_T46_pipeline.nii.gz")
+mask_2_path = os.path.join(base_path, f"ses{session}/manual_brainmask/exp_param", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_T46_pipeline_mask.nii.gz")
 
 volume1_data = nib.load(vol_1_path).get_fdata()
 volume2_data = nib.load(vol_2_path).get_fdata()
@@ -19,16 +19,13 @@ volume2_data = nib.load(vol_2_path).get_fdata()
 mask1_data = nib.load(mask_1_path).get_fdata()
 mask2_data = nib.load(mask_2_path).get_fdata()
 
-print(mask1_data.shape)
-print(mask2_data.shape)
-
 volume1_data = volume1_data * mask1_data
 volume2_data = volume2_data * mask2_data
 
 volume1_fft = np.fft.fftn(volume1_data)
 volume2_fft = np.fft.fftn(volume2_data)
 
-# Calculer le spectre de puissance  
+# Calculer le spectre de puissance
 volume1_power_spectrum = np.abs(volume1_fft)**2
 volume2_power_spectrum = np.abs(volume2_fft)**2
 
