5c157c0fd51eac5939e7a9c53cea5334e5a131f7
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-20 10:06:08 +0100

    for meso

--- nifty_module/06_reconstruction_snapshots.py
+++ nifty_module/06_reconstruction_snapshots.py
@@ -14,9 +14,10 @@ from sklearn.preprocessing import StandardScaler
 def apply_pca(vol1, vol2, n_components=2):
     vol1_flat = vol1.flatten().reshape(1, -1)
     vol2_flat = vol2.flatten().reshape(1, -1)
-    
+
     print(vol1_flat.shape, vol2_flat.shape)
     data = np.stack([vol1_flat, vol2_flat])
+    print(data.shape)
 
     scaler = StandardScaler()
     data_normalized = scaler.fit_transform(data)
