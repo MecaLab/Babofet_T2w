3149564c0a40188eb20bb63b5ce812187e6e96f9
leroux.b <baptiste.leroux@univ-amu.fr>
2025-03-31 09:43:28 +0200

    for meso

--- nifty_module/05_image_reconstruction.py
+++ nifty_module/05_image_reconstruction.py
@@ -88,9 +88,7 @@ if __name__ == "__main__":
     elif mask_model == "mattia":
         bm_folder = "mattia_masks"
 
-    list_subjs = ["sub-Fabienne_ses-01", "sub-Fabienne_ses-05", "sub-Fabienne_ses-09",
-                  "sub-Aziza_ses-01", "sub-Aziza_ses-05", "sub-Aziza_ses-09"
-                  "sub-Formule_ses-01", "sub-Formule_ses-05"]
+    list_subjs = ["sub-Formule_ses-01"]
 
     for subject in subject_IDs:
         if subject not in list_subjs:
