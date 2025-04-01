1a083eeb5d8d3478468ca956a3801a72cdf3d0f2
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-01 15:20:51 +0200

    for meso

--- tools/tmp.py
+++ tools/tmp.py
@@ -38,40 +38,63 @@ def world_to_voxel(world_coords, affine_matrix):
 
 
 # Sélection d'une coupe sagittale
-y_index_vol1 = vol1_data.shape[1] // 2
+voxel_y_index = vol1_data.shape[1] // 2
+voxel_coords_vol1 = np.array([
+    [0, voxel_y_index, 0],
+    [vol1_data.shape[0]-1, voxel_y_index, vol1_data.shape[2]-1]
+])
 
-# Conversion en coordonnées mondiales
-voxel_coords_vol1 = np.array([[0, y_index_vol1, 0], [0, y_index_vol1, vol1_data.shape[2]-1]])
+# Convertir les coordonnées voxel en coordonnées mondiales
 world_coords = voxel_to_world(voxel_coords_vol1, affine_matrix_vol1)
 
-# Conversion en coordonnées voxel pour le second volume
+# Convertir les coordonnées mondiales en coordonnées voxel dans le second volume
 voxel_coords_vol2 = world_to_voxel(world_coords, affine_matrix_vol2)
-y_index_vol2 = int(round(voxel_coords_vol2[0, 1]))
 
-# Extraire les coupes
-slice_2d_vol1 = vol1_data[:, y_index_vol1, :]
-slice_2d_vol2 = vol2_data[:, y_index_vol2, :]
+# Extraire les tranches 2D correspondantes dans les deux volumes
+slice_y_index_vol1 = int(round(voxel_coords_vol1[0, 1]))
+slice_y_index_vol2 = int(round(voxel_coords_vol2[0, 1]))
 
-# Déterminer la position de la ligne rouge dans le premier volume
-line_z_index_vol1 = slice_2d_vol1.shape[1] // 2
-line_world_coords = voxel_to_world(np.array([[0, y_index_vol1, line_z_index_vol1]]), affine_matrix_vol1)
+slice_2d_vol1 = vol1_data[:, slice_y_index_vol1, :]
+slice_2d_vol2 = vol2_data[:, slice_y_index_vol2, :]
+
+# Position de la ligne horizontale dans le premier volume
+line_position_vol1 = 50
+
+# Convertir la position de la ligne en coordonnées mondiales
+line_world_coords = voxel_to_world(np.array([[0, line_position_vol1, 0], [vol1_data.shape[0]-1, line_position_vol1, vol1_data.shape[2]-1]]), affine_matrix_vol1)
+
+# Convertir en coordonnées voxel dans le second volume
 line_voxel_coords_vol2 = world_to_voxel(line_world_coords, affine_matrix_vol2)
-line_z_index_vol2 = int(round(line_voxel_coords_vol2[0, 2]))
 
-# Affichage
-fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+# Position de la ligne horizontale dans le second volume
+line_position_vol2 = int(round(line_voxel_coords_vol2[0, 1]))
+
+# Extraire les intensités le long de la ligne rouge
+intensity_profile_vol1 = slice_2d_vol1[:, line_position_vol1]
+intensity_profile_vol2 = slice_2d_vol2[:, line_position_vol2]
+
+# Affichage des tranches et des courbes d'intensité
+fig, axes = plt.subplots(3, 1, figsize=(12, 9))
+
 axes[0].imshow(slice_2d_vol1.T, cmap='gray', origin='lower')
-axes[0].set_title(f'Volume 1 - Slice at Y = {y_index_vol1}')
+axes[0].set_title(f'Volume 1 - Slice at Y = {voxel_y_index}')
 axes[0].set_xlabel('X')
 axes[0].set_ylabel('Z')
-axes[0].plot([0, slice_2d_vol1.shape[0]-1], [line_z_index_vol1, line_z_index_vol1], color='red', linewidth=2)
+axes[0].plot([0, slice_2d_vol1.shape[0]-1], [line_position_vol1, line_position_vol1], color='red', linewidth=2)
 
 axes[1].imshow(slice_2d_vol2.T, cmap='gray', origin='lower')
 axes[1].set_title(f'Volume 2 - Slice at corresponding Y')
 axes[1].set_xlabel('X')
 axes[1].set_ylabel('Z')
-axes[1].plot([0, slice_2d_vol2.shape[0]-1], [line_z_index_vol2, line_z_index_vol2], color='red', linewidth=2)
+axes[1].plot([0, slice_2d_vol2.shape[0]-1], [line_position_vol2, line_position_vol2], color='red', linewidth=2)
 
+# Affichage des courbes d'intensité
+axes[2].plot(intensity_profile_vol1, label='Volume 1', color='blue')
+axes[2].plot(intensity_profile_vol2, label='Volume 2', color='green')
+axes[2].set_title('Intensity Profiles along the Red Line')
+axes[2].set_xlabel('X')
+axes[2].set_ylabel('Intensity')
+axes[2].legend()
 
 plt.tight_layout()
 plt.savefig("tmp.png")
