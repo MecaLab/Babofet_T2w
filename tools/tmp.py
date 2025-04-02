029484e8293236920e14290f58474b8ce9b6acfd
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-02 11:34:54 +0200

    for meso

--- tools/tmp.py
+++ tools/tmp.py
@@ -26,71 +26,59 @@ affine_matrix_vol1 = vol1.affine
 affine_matrix_vol2 = vol2.affine
 
 
+# Fonction pour convertir les coordonnées voxel en coordonnées mondiales
 def voxel_to_world(voxel_coords, affine_matrix):
-    # Fonction pour convertir les coordonnées voxel en coordonnées mondiales
     homogeneous_coords = np.concatenate([voxel_coords, np.ones((voxel_coords.shape[0], 1))], axis=1)
     world_coords = np.dot(affine_matrix, homogeneous_coords.T).T
     return world_coords[:, :3]
 
 
+# Fonction pour convertir les coordonnées mondiales en coordonnées voxel
 def world_to_voxel(world_coords, affine_matrix):
-    # Fonction pour convertir les coordonnées mondiales en coordonnées voxel
     inv_affine_matrix = np.linalg.inv(affine_matrix)
     homogeneous_coords = np.concatenate([world_coords, np.ones((world_coords.shape[0], 1))], axis=1)
     voxel_coords = np.dot(inv_affine_matrix, homogeneous_coords.T).T
     return np.round(voxel_coords[:, :3]).astype(int)
 
 
-voxel_y_index = vol1_data.shape[1] // 2
-voxel_coords_vol1 = np.array([
-    [0, voxel_y_index, 0],
-    [vol1_data.shape[0]-1, voxel_y_index, vol1_data.shape[2]-1]
-])
+idxs = [50, 60, 70, 80]
 
-# Convertir les coordonnées voxel en coordonnées mondiales
-world_coords = voxel_to_world(voxel_coords_vol1, affine_matrix_vol1)
+fig, axes = plt.subplots(len(idxs), 3, figsize=(12, 3 * len(idxs)))
 
-# Ajuster les coordonnées mondiales en tenant compte du décalage de 0.25 mm sur la composante Y
-world_coords[:, 1] += 0.25
+for i, idx in enumerate(idxs):
+    voxel_y_index = vol1_data.shape[1] // 2
+    voxel_coords_vol1 = np.array([[vol1_data.shape[0] // 2, voxel_y_index, idx]])
+    world_coords = voxel_to_world(voxel_coords_vol1, affine_matrix_vol1)
+    voxel_coords_vol2 = world_to_voxel(world_coords, affine_matrix_vol2)
 
-# Convertir les coordonnées mondiales ajustées en coordonnées voxel dans le second volume
-voxel_coords_vol2 = world_to_voxel(world_coords, affine_matrix_vol2)
+    slice_2d_vol1 = vol1_data[:, voxel_y_index, :]
+    slice_2d_vol2 = vol2_data[:, voxel_coords_vol2[0, 1], :]
 
-# Extraire les tranches 2D correspondantes dans les deux volumes
-slice_y_index_vol1 = int(round(voxel_coords_vol1[0, 1]))
-slice_y_index_vol2 = int(round(voxel_coords_vol2[0, 1]))
+    mask_2d_vol1 = brainmask1[:, voxel_y_index, :]
+    mask_2d_vol2 = brainmask2[:, voxel_coords_vol2[0, 1], :]
 
-slice_2d_vol1 = vol1_data[:, slice_y_index_vol1, :]
-slice_2d_vol2 = vol2_data[:, slice_y_index_vol2, :]
+    line_position_voxel1 = voxel_coords_vol1[0, 0]
+    line_world_coords = voxel_to_world(np.array([[line_position_voxel1, voxel_y_index, 0]]), affine_matrix_vol1)
+    line_voxel_coords_vol2 = world_to_voxel(line_world_coords, affine_matrix_vol2)
+    line_position_voxel2 = line_voxel_coords_vol2[0, 0]
 
-# Position de la ligne horizontale à afficher dans le premier volume
-line_position_vol1 = 50
+    intensity_profile_vol1 = slice_2d_vol1[line_position_voxel1, :] * mask_2d_vol1[line_position_voxel1, :]
+    intensity_profile_vol2 = slice_2d_vol2[line_position_voxel2, :] * mask_2d_vol2[line_position_voxel2, :]
 
-# Convertir la position de la ligne en coordonnées mondiales
-line_world_coords = voxel_to_world(np.array([[0, line_position_vol1, 0], [vol1_data.shape[0]-1, line_position_vol1, vol1_data.shape[2]-1]]), affine_matrix_vol1)
+    axes[i, 0].imshow(slice_2d_vol1.T, cmap='gray', origin='lower')
+    axes[i, 0].set_title(f'Vol 1 - Slice at Y={voxel_y_index}, Z={idx}')
+    axes[i, 0].plot([0, slice_2d_vol1.shape[0]-1], [line_position_voxel1, line_position_voxel1], color='red', linewidth=2)
 
-# Ajuster les coordonnées mondiales de la ligne en tenant compte du décalage de 0.25 mm sur la composante Y
-line_world_coords[:, 1] += 0.25
+    axes[i, 1].imshow(slice_2d_vol2.T, cmap='gray', origin='lower')
+    axes[i, 1].set_title(f'Vol 2 - Slice at corresponding Y, Z={idx}')
+    axes[i, 1].plot([0, slice_2d_vol2.shape[0]-1], [line_position_voxel2, line_position_voxel2], color='red', linewidth=2)
 
-# Convertir les coordonnées mondiales de la ligne en coordonnées voxel dans le second volume
-line_voxel_coords_vol2 = world_to_voxel(line_world_coords, affine_matrix_vol2)
+    axes[i, 2].plot(intensity_profile_vol1, label='Volume 1', color='blue')
+    axes[i, 2].plot(intensity_profile_vol2, label='Volume 2', color='green')
+    axes[i, 2].set_title(f'Intensity Profile at Z={idx}')
+    axes[i, 2].set_xlabel('Z')
+    axes[i, 2].set_ylabel('Intensity')
+    axes[i, 2].legend()
 
-# Position de la ligne horizontale à afficher dans le second volume
-line_position_vol2 = int(round(line_voxel_coords_vol2[0, 1]))
-
-# Afficher les deux tranches 2D côte à côte avec une ligne horizontale à la même position
-fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-
-axes[0].imshow(slice_2d_vol1.T, cmap='gray', origin='lower')
-axes[0].set_title(f'Volume 1 - Slice at Y = {voxel_y_index}')
-axes[0].set_xlabel('X')
-axes[0].set_ylabel('Z')
-axes[0].plot([0, slice_2d_vol1.shape[0]-1], [line_position_vol1, line_position_vol1], color='red', linewidth=2)
-
-axes[1].imshow(slice_2d_vol2.T, cmap='gray', origin='lower')
-axes[1].set_title(f'Volume 2 - Slice at corresponding Y')
-axes[1].set_xlabel('X')
-axes[1].set_ylabel('Z')
-axes[1].plot([0, slice_2d_vol2.shape[0]-1], [line_position_vol2, line_position_vol2], color='red', linewidth=2)
-
+plt.tight_layout()
 plt.savefig("tmp.png")
