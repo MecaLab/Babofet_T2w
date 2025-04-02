0c4b5d272c63df29d288a21ae6dc421b6c399f36
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-02 12:01:57 +0200

    for meso

--- tools/tmp.py
+++ tools/tmp.py
@@ -19,33 +19,32 @@ vol2 = nib.load(vol_2_path)
 brainmask1 = nib.load(mask_1_path).get_fdata()
 brainmask2 = nib.load(mask_2_path).get_fdata()
 
-vol1_data = vol1.get_fdata()
-vol2_data = vol2.get_fdata()
+data1 = vol1.get_fdata()
+data2 = vol2.get_fdata()
 
-affine_matrix_vol1 = vol1.affine
-affine_matrix_vol2 = vol2.affine
+affine1 = vol1.affine
+affine2 = vol2.affine
 
-shape1 = vol1_data.shape
-shape2 = vol2_data.shape
+shape1 = data1.shape
+shape2 = data2.shape
 
 # Déterminer le nombre de coupes sagittales
 # Calculer l'indice du milieu de l'axe sagittal pour le volume 1
 mid_sagittal_index1 = shape1[1] // 2
 
-# Indices axiaux à afficher
+# Indices axiaux (z) à afficher
 axial_indices = [50, 60, 70]
 
 # Afficher les coupes sagittales pour chaque indice axial
 for axial_index in axial_indices:
-    # Coordonnées du voxel au milieu de l'axe sagittal et à l'indice axial donné pour le volume 1
-    voxel_coords1 = np.array([shape1[0] // 2, shape1[1] // 2, mid_sagittal_index1, 1])
-    voxel_coords1[1] = axial_index  # Mettre à jour la composante axiale
+    # Coordonnées du voxel au milieu de l'axe sagittal (y) et à l'indice axial (z) donné pour le volume 1
+    voxel_coords1 = np.array([shape1[0] // 2, mid_sagittal_index1, axial_index, 1])
 
     # Convertir en coordonnées mondiales
-    world_coords = affine_matrix_vol1 @ voxel_coords1
+    world_coords = affine1 @ voxel_coords1
 
     # Convertir les coordonnées mondiales en coordonnées de voxels dans le volume 2
-    voxel_coords2 = np.linalg.inv(affine_matrix_vol2) @ world_coords
+    voxel_coords2 = np.linalg.inv(affine2) @ world_coords
     voxel_coords2 = np.round(voxel_coords2[:3]).astype(int)
 
     # Vérifier que les coordonnées sont dans les limites du volume 2
@@ -53,13 +52,13 @@ for axial_index in axial_indices:
         plt.figure(figsize=(10, 5))
 
         plt.subplot(1, 2, 1)
-        plt.imshow(vol1_data[:, axial_index, :].T, cmap='gray', origin='lower')
-        plt.title(f'Volume 1 - Axial Slice {axial_index}')
+        plt.imshow(data1[:, mid_sagittal_index1, axial_index].T, cmap='gray', origin='lower')
+        plt.title(f'Volume 1 - Sagittal Slice {mid_sagittal_index1}, Axial Index {axial_index}')
         plt.axis('off')
 
         plt.subplot(1, 2, 2)
-        plt.imshow(vol2_data[:, voxel_coords2[1], :].T, cmap='gray', origin='lower')
-        plt.title(f'Volume 2 - Axial Slice {voxel_coords2[1]}')
+        plt.imshow(data2[:, voxel_coords2[1], voxel_coords2[2]].T, cmap='gray', origin='lower')
+        plt.title(f'Volume 2 - Sagittal Slice {voxel_coords2[1]}, Axial Index {voxel_coords2[2]}')
         plt.axis('off')
 
 plt.savefig("tmp.png")
