c78aff2a98ef946fd4197d61ad6fe086b8a6d899
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-02 11:51:13 +0200

    for meso

--- tools/tmp.py
+++ tools/tmp.py
@@ -33,21 +33,33 @@ num_slices1 = shape1[2]
 num_slices2 = shape2[2]
 
 # Calculer les indices des coupes à afficher
-indices1 = np.linspace(0, num_slices1 - 1, num=min(num_slices1, num_slices2), dtype=int)
-indices2 = np.linspace(0, num_slices2 - 1, num=min(num_slices1, num_slices2), dtype=int)
+num_slices1 = shape1[2]
+indices1 = np.linspace(0, num_slices1 - 1, num=min(num_slices1, shape2[2]), dtype=int)
 
 # Afficher les coupes sagittales
-for i in range(min(num_slices1, num_slices2)):
-    plt.figure(figsize=(10, 5))
+for i in range(min(num_slices1, shape2[2])):
+    # Coordonnées du voxel dans le volume 1
+    voxel_coords1 = np.array([shape1[0] // 2, shape1[1] // 2, indices1[i], 1])
 
-    plt.subplot(1, 2, 1)
-    plt.imshow(vol1_data[:, :, indices1[i]].T, cmap='gray', origin='lower')
-    plt.title(f'Volume 1 - Slice {indices1[i]}')
-    plt.axis('off')
+    # Convertir en coordonnées mondiales
+    world_coords = affine_matrix_vol1 @ voxel_coords1
 
-    plt.subplot(1, 2, 2)
-    plt.imshow(vol2_data[:, :, indices2[i]].T, cmap='gray', origin='lower')
-    plt.title(f'Volume 2 - Slice {indices2[i]}')
-    plt.axis('off')
+    # Convertir les coordonnées mondiales en coordonnées de voxels dans le volume 2
+    voxel_coords2 = np.linalg.inv(affine_matrix_vol2) @ world_coords
+    voxel_coords2 = np.round(voxel_coords2[:3]).astype(int)
+
+    # Vérifier que les coordonnées sont dans les limites du volume 2
+    if (0 <= voxel_coords2[2] < shape2[2]):
+        plt.figure(figsize=(10, 5))
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(vol1_data[:, :, indices1[i]].T, cmap='gray', origin='lower')
+        plt.title(f'Volume 1 - Slice {indices1[i]}')
+        plt.axis('off')
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(vol2_data[:, :, voxel_coords2[2]].T, cmap='gray', origin='lower')
+        plt.title(f'Volume 2 - Slice {voxel_coords2[2]}')
+        plt.axis('off')
 
 plt.savefig("tmp.png")
