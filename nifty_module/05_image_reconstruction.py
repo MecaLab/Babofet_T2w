e594a9c6e07d225924e821fb986891c0b1cb7b0e
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-03 10:02:09 +0200

    for meso

--- nifty_module/05_image_reconstruction.py
+++ nifty_module/05_image_reconstruction.py
@@ -91,9 +91,9 @@ if __name__ == "__main__":
     SUFFIX_EXP = "_ISO3"  # need to be updated for every exp
 
     list_subjs = [
-        "sub-Fabienne_ses-01", "sub-Fabienne_ses-05", "sub-Fabienne_ses-09"
-        "sub-Aziza_ses-01", "sub-Aziza_ses-05", "sub-Aziza_ses-09",
-        "sub-Formule_ses-01", "sub-Formule_ses-05", "sub-Formule_ses-09",
+        "sub-Fabienne_ses-09"
+        # "sub-Aziza_ses-01", "sub-Aziza_ses-05", "sub-Aziza_ses-09",
+        # "sub-Formule_ses-01", "sub-Formule_ses-05", "sub-Formule_ses-09",
     ]
 
     for subject in subject_IDs:
