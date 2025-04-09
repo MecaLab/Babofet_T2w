0f499f51b714e594c16a0b8dc5d81fcab59ca22a
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-09 10:00:00 +0200

    for meso

--- tools/mask_rcnn.py
+++ tools/mask_rcnn.py
@@ -1,7 +1,12 @@
-import numpy as np
 import nibabel as nib
-import mrcnn.model as modellib
-from mrcnn import utils
-from mrcnn.config import Config
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from torchvision.models.detection.mask_rcnn import MaskRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+import transforms as T
 
 print("OK")
