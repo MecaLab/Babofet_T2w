6e2997348d86851070e9ab6b4c94c5c7688e0f3f
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-10 14:13:40 +0200

    for meso

--- tools/mask_rcnn.py
+++ tools/mask_rcnn.py
@@ -12,21 +12,45 @@ subjects = ["Aziza"]
 sessions = ["01"]
 
 
-def get_data_for_subj(subject, session):
+
+def get_data_for_subj(i, subject, session):
     full_subject_name = f"sub-{subject}_ses-{session}"
     subject_path = os.path.join(cfg.MESO_OUTPUT_PATH, full_subject_name)
 
     subject_anat_path = os.path.join(subject_path, "denoising")
     subject_bm_path = os.path.join(subject_path, "manual_masks")
 
+    datas = []
+
+    subj_name = f"{subject}_{session}"
+    cpt_orientation = {}
     for anat_file in os.listdir(subject_anat_path):
         if "HASTE" in anat_file:
             anat_file_path = os.path.join(subject_anat_path, anat_file)
             bm_file_path = os.path.join(subject_bm_path, anat_file.replace(".nii", "_mask.nii.gz"))
-            print(os.path.exists(anat_file_path), os.path.exists(bm_file_path), bm_file_path)
+
+            orientation = anat_file.split("HASTE")[-1].split("_")[1]
+            if 'AX' in orientation:
+                orientation = "axial"
+            elif "SAG" in orientation:
+                orientation = "sagittal"
+            elif "COR" in orientation:
+                orientation = "coronal"
+
+            if orientation not in cpt_orientation:
+                cpt_orientation[orientation] = 1
+            else:
+                cpt_orientation[orientation] += 1
+
+            new_filename_anat = f"{subj_name}_{orientation}_{cpt_orientation[orientation]}.nii"
+            new_filename_bm = f"{subj_name}_{orientation}_{cpt_orientation[orientation]}_mask.nii.gz"
+
+            print(new_filename_anat, new_filename_bm)
+
+            break
 
 
 if __name__ == "__main__":
-    for subject in subjects:
+    for i, subject in enumerate(subjects):
         for session in sessions:
             get_data_for_subj(subject, session)
