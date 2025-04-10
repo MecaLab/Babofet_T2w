ec17759a3b690041b77383759a3d9baa660e7140
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-10 13:44:34 +0200

    for meso

--- tools/mask_rcnn.py
+++ tools/mask_rcnn.py
@@ -1,12 +1,26 @@
-import nibabel as nib
-import numpy as np
 import os
-from sklearn.model_selection import train_test_split
-import torch
-from torch.utils.data import Dataset, DataLoader
-import torchvision
-from torchvision.models.detection.mask_rcnn import MaskRCNN
-from torchvision.models.detection.rpn import AnchorGenerator
-import transforms as T
+import sys
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
 
-print("OK")
+output_folder = os.path.join(cfg.BASE_PATH, "Mask_RCNN", "MRI_dataset")
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+subjects = ["Aziza"]
+
+
+def get_data_for_subj(subject):
+    subject_path = os.path.join(cfg.MESO_OUTPUT_PATH, subject)
+
+    subject_anat_path = os.path.join(subject_path, "denoising")
+    subject_bm_path = os.path.join(subject_path, "manual_brainmask")
+
+    for anat_file, bm_file in zip(os.listdir(subject_anat_path, subject_bm_path)):
+        print(anat_file, bm_file)
+
+
+if __name__ == "__main__":
+    for subject in subjects:
+        get_data_for_subj(subject)
