6597e95a5fd2060ef1d727e95ab477ab3c15a49c
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-10 15:54:28 +0200

    for meso

--- tools/tmp_maskrcnn.py
+++ tools/tmp_maskrcnn.py
@@ -4,6 +4,10 @@ import torch
 import torchvision
 from torchvision.io import read_image
 from torchvision.ops.boxes import masks_to_boxes
+from torchvision.transforms import v2 as T
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
 sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 
@@ -55,4 +59,34 @@ class BrainDataset(torch.utils.data.Dataset):
 
 
 custom_ds = BrainDataset(root=base_path, transforms=None)
-print(custom_ds[0])
+
+
+def get_model_instance_segmentation(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 32
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(
+        in_features_mask,
+        hidden_layer,
+        num_classes
+    )
+
+    return model
+
+
+def get_transform(train):
+    transforms = []
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    transforms.append(T.ToDtype(torch.float, scale=True))
+    transforms.append(T.ToPureTensor())
+    return T.Compose(transforms)
