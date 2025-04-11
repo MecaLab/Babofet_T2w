d2731aade39f1a2207faf0ed15fc65aee9b36009
leroux.b <baptiste.leroux@univ-amu.fr>
2025-04-11 09:58:17 +0200

    for meso

---
+++ tools/mv_fetalbet_masks.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+source_folder = "/scratch/lbaptiste/data/dataset/babofet/fetalBet_masks_V2"
+
+main_dst_folder = "scratch/lbaptiste/data/dataset/babofet/derivatives"
+
+for subj in os.listdir(source_folder):
+    subj_path_src = os.path.join(source_folder, subj)
+
+    subj_path_dst = os.path.join(main_dst_folder, subj)
+
+    folder_output = os.path.join(subj_path_dst, "fetalBet_masks_v2")
+
+    if not os.path.exists(folder_output):
+        os.makedirs(folder_output)
+
+    for file in os.listdir(subj_path_src):
+        if "HASTE" in file:
+            file_path = os.path.join(subj_path_src, file)
+
+            shutil.copy2(file_path, os.path.join(folder_output, file))
+
+    print(f"OK for {subj}")
+    break
