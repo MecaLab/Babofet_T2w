eb5a2784fdb29bcd8b2ae425bff4efc38135589c
leroux.b <baptiste.leroux@univ-amu.fr>
2025-05-15 11:37:27 +0200

    for meso

---
+++ nnunet_module/a_process_file.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
+
+subject = "Aziza"
+
+input_path = os.path.join(cfg.SEG_INPUT_PATH, subject)
+output_path = os.path.join(cfg.NNUNET_RAW_PATH, "imagesTr")
+
+for session in os.listdir(input_path):
+    recons_file = os.path.join(input_path, session, "reo-SVR-output-brain_rhesus.nii.gz")
+
+    if not os.path.exists(recons_file):
+        print(f"File {recons_file} does not exist.")
+        continue
+
+    print(f"Processing {recons_file}...")
+
+    output_filename = f"{subject.lower()}_0{session[3:]}.nii.gz"
+    print(output_filename)
+
+
+
+
+
