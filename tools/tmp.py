ff65192e096fd910380306c52ae09efcd0ec28b3
leroux.b <baptiste.leroux@univ-amu.fr>
2025-05-26 15:16:46 +0200

    for meso

--- tools/tmp.py
+++ tools/tmp.py
@@ -1,115 +1,101 @@
 import os
 import nibabel as nib
+import qc_recons as qc_recons
 from nibabel.processing import resample_from_to
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.measure import profile_line
-
-subject = "Aziza"
-base_path = f"../data/recons_folder/{subject}/"
-sessions = ["01", "05", "09"]
-
-views = ["coronal", "axial", "sagital"]
+from scipy import stats
 
 
-for session in sessions:
-    vol_1_path = os.path.join(base_path, f"ses{session}/manual_brainmask", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_pipeline.nii.gz")
-    mask_1_path = os.path.join(base_path, f"ses{session}/manual_brainmask", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_pipeline_mask.nii.gz")
+def normalize_min_max(volume):
+    return (volume - volume.min()) / (volume.max() - volume.min())
 
-    vol_2_path = os.path.join(base_path, f"ses{session}/mattia_brainmask", f"sub-{subject}_ses-{session}_haste_3DHR_mattia_bm_pipeline.nii.gz")
-    mask_2_path = os.path.join(base_path, f"ses{session}/mattia_brainmask", f"sub-{subject}_ses-{session}_haste_3DHR_mattia_bm_pipeline_mask.nii.gz")
 
-    """vol_2_path = os.path.join(base_path, f"ses{session}/manual_brainmask/exp_param", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_ISO4_pipeline.nii.gz")
-    mask_2_path = os.path.join(base_path, f"ses{session}/manual_brainmask/exp_param", f"sub-{subject}_ses-{session}_haste_3DHR_manual_bm_ISO4_pipeline_mask.nii.gz")"""
+def freedman_diaconis_bins(data):
+    """Calcule le nombre optimal de bins selon la règle de Freedman-Diaconis."""
+    q75, q25 = np.percentile(data, [75, 25])
+    iqr = q75 - q25
+    n = len(data)
+    bin_width = 2 * iqr / (n ** (1/3))
+    return int((data.max() - data.min()) / bin_width)
 
-    vol1 = nib.load(vol_1_path)
-    vol2 = nib.load(vol_2_path)
 
-    mask1_data = nib.load(mask_1_path).get_fdata()
-    mask_data2 = nib.load(mask_2_path)
+def plot_histo(vol1_masked, vol2_masked):
 
-    data1 = vol1.get_fdata()
+    vol1 = normalize_min_max(vol1_masked)
+    vol2 = normalize_min_max(vol2_masked)
 
-    nii2_resampled = resample_from_to(vol2, vol1)
-    data2_resampled = nii2_resampled.get_fdata()
+    hist_range = (min(vol1.min(), vol2.min()), max(vol1.max(), vol2.max()))
+    bins = freedman_diaconis_bins(np.concatenate([vol1, vol2]))
 
-    mask2_resampled = resample_from_to(mask_data2, vol1, order=0).get_fdata()
+    hist1, bins1 = np.histogram(vol1, bins=bins, density=True, range=hist_range)
+    hist2, bins2 = np.histogram(vol2, bins=bins, density=True, range=hist_range)
 
-    view_idxs = [25, 50, 65, 75, 90]
+    bin_centers = (bins1[:-1] + bins1[1:]) / 2  # Centres des bins
+    wasserstein_dist = stats.wasserstein_distance(bin_centers, bin_centers, hist1 * np.diff(bins1),
+                                                  hist2 * np.diff(bins2))
 
-    for view in views:
-        for view_idx in view_idxs:
-            png_filename = f"comparaison_{subject.lower()}_ses-{session}_{view}_{view_idx}.png"
-            if view == "axial":
-                slice_idx = view_idx  # data1.shape[2]//2  # Coupe axiale
-                slice_data1 = data1[:, :, slice_idx]
-                slice_data_resampled = data2_resampled[:, :, slice_idx]
-                slice_mask1 = mask1_data[:, :, slice_idx]
-                slice_mask2 = mask2_resampled[:, :, slice_idx]
-            elif view == "sagital":
-                slice_idx = view_idx  # data1.shape[1] // 2  # Coupe sagital
-                slice_data1 = data1[:, slice_idx, :]
-                slice_data_resampled = data2_resampled[:, slice_idx, :]
-                slice_mask1 = mask1_data[:, slice_idx, :]
-                slice_mask2 = mask2_resampled[:, slice_idx, :]
-            elif view == "coronal":
-                slice_idx = view_idx  # data1.shape[0] // 2  # Coupe sagital
-                slice_data1 = data1[slice_idx, :, :]
-                slice_data_resampled = data2_resampled[slice_idx, :, :]
-                slice_mask1 = mask1_data[slice_idx, :, :]
-                slice_mask2 = mask2_resampled[slice_idx, :, :]
+    title = f"{subject} {session}"
+    output_path = "C:/Users/leroux.b/Pictures"
+    output_filename = f"{subject}_{session}_comparison.png"
+    plt.figure(figsize=(10, 6), facecolor='white')
+    plt.plot(bin_centers, hist1, label='Niftymic', linestyle='-', alpha=0.7)
+    plt.plot(bin_centers, hist2, label="Nesvor", linestyle='--', alpha=0.7)
+    plt.title(f"{title}\nWasserstein Distance: {wasserstein_dist:.4f}\nBins: {bins}")
+    plt.xlabel("Intensité")
+    plt.ylabel("Densité")
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_path, output_filename))
+    plt.close()
 
-            # Définir plusieurs lignes horizontales à analyser
-            """y_values = [50, 60, 70, 80, 90]  # Plusieurs valeurs de y
-            x1, x2 = 0, 120  # Début et fin de la ligne"""
-            y_values = np.where(np.any(slice_mask1 > 0, axis=1))[0]  # Prend les lignes contenant du masque
-            if len(y_values) > 3:
-                y_values = y_values[::max(1, len(y_values) // 3)]  # Évite un pas de 0
 
-            # Déterminer le nombre de lignes
+if __name__ == "__main__":
 
-            if len(y_values) == 0:
-                print(f"Aucune ligne trouvée pour {view} à l'indice {view_idx}, masque vide ?")
-                continue  # Passe à l'itération suivante
+    subject = "Fabienne"
 
-            n_rows = len(y_values)
-            fig, axes = plt.subplots(n_rows, 4, figsize=(19, 5 * n_rows))
+    uncorrected_bias_path = f"W:/meca/data/babofet_DB/2024_new_stuff/bounti/svrtk_BOUNTI/input_SRR_niftymic/haste/{subject}"
+    corrected_bias_path = f"W:/meca/data/babofet_DB/2024_new_stuff/bounti/svrtk_BOUNTI/input_BOUNTI_seg/haste/{subject}_bc"
 
-            for i, y in enumerate(y_values):
-                x1, x2 = np.where(slice_mask1[y, :] > 0)[0][[0, -1]]
+    for session in os.listdir(uncorrected_bias_path):
+        print(session)
 
-                # Extraire les profils d'intensité pour cette ligne y
-                intensity1 = profile_line(slice_data1, (y, x1), (y, x2))
-                intensity2 = profile_line(slice_data_resampled, (y, x1), (y, x2))
-                intensity_diff = np.subtract(intensity1, intensity2)
-                intensity_diff_percent = 100 * (np.array(intensity1) - np.array(intensity2)) / np.array(intensity1)
-
-                axes[i, 0].imshow(slice_data1, cmap="gray")
-                axes[i, 0].imshow(slice_mask1, cmap="Reds", alpha=0.3)  # Masque en transparence
-                axes[i, 0].plot([x1, x2], [y, y], 'r-')  # Ligne rouge sur la coupe
-                axes[i, 0].set_title(f"Coupe originale {view_idx} with y={y}")
-
-                axes[i, 1].imshow(slice_data_resampled, cmap="gray")
-                axes[i, 1].imshow(slice_mask2, cmap="Reds", alpha=0.3)  # Masque en transparence
-                axes[i, 1].plot([x1, x2], [y, y], 'r-')  # Ligne rouge sur la coupe
-                axes[i, 1].set_title(f"Coupe rééchantillonnée {view_idx} with y={y}")
-
-                axes[i, 2].plot(intensity1, label="Original", linestyle="dashed")
-                axes[i, 2].plot(intensity2, label="Resampled", linestyle="solid")
-                axes[i, 2].set_xlabel("Position le long de la ligne")
-                axes[i, 2].set_ylabel("Intensité")
-                axes[i, 2].legend()
-                axes[i, 2].grid()
-                axes[i, 2].set_title(f"Profil d'intensité (y={y})")
-
-                axes[i, 3].plot(intensity_diff_percent, label="Variation (%)", color="purple")
-                axes[i, 3].axhline(0, color="black", linestyle="--", label="Zéro Variation")  # Ligne de base à zéro
-                axes[i, 3].set_xlabel("Position le long de la ligne")
-                axes[i, 3].set_ylabel("Variation d'intensité (%)")
-                axes[i, 3].grid()
-                axes[i, 3].set_title(f"Variation en pourcentage (y={y})")
-
-            # Affichage global
-            plt.tight_layout()
-            plt.savefig(png_filename)
-            plt.close()
+    
+    """
+    subject = "Formule"
+    
+    niftymic_path = f"W:/meca/data/babofet_DB/2024_new_stuff/bounti/svrtk_BOUNTI/input_SRR_niftymic/haste/{subject}"
+    niftymic_bm_path = f"W:/meca/data/babofet_DB/2024_new_stuff/bounti/svrtk_BOUNTI/output_BOUNTI_seg/haste/{subject}"
+    nesvor_path = f"C:/Users/leroux.b/Documents/Archive (1)/res-{subject}"
+    
+    
+    for session in os.listdir(niftymic_path):
+        print(f"Computing session {session}")
+        niftymic_file = os.path.join(niftymic_path, session, "reo-SVR-output-brain_rhesus.nii.gz")
+        if not os.path.exists(niftymic_file):
+            print(f"File {niftymic_file} does not exist, skipping...")
+            continue
+        niftymic_bm_file = os.path.join(niftymic_bm_path, session, "reo-SVR-output-brain_rhesus-mask-bet-1.nii.gz")
+    
+        nesvor_filename = f"sub-{subject}_ses-{session[3:]}_T2_HASTE_nesvor_rigide.nii.gz"
+        nesvor_bm_filename = f"sub-{subject}_ses-{session[3:]}_T2_HASTE_nesvor_rigide-mask.nii.gz"
+    
+        nesvor_file = os.path.join(nesvor_path, nesvor_filename)
+        if not os.path.exists(nesvor_file):
+            print(f"File {nesvor_file} does not exist, skipping...")
+            continue
+    
+        nesvor_bm_file = os.path.join(nesvor_path, nesvor_bm_filename)
+    
+        niftymic_data = nib.load(niftymic_file).get_fdata()
+        nesvor_data = nib.load(nesvor_file).get_fdata()
+    
+        niftymic_bm_data = nib.load(niftymic_bm_file).get_fdata()
+        nesvor_bm_data = nib.load(nesvor_bm_file).get_fdata()
+    
+        vol1_masked = niftymic_data[niftymic_bm_data > 0]
+        vol2_masked = nesvor_data[nesvor_bm_data > 0]
+    
+        plot_histo(vol1_masked, vol2_masked)
+        """
