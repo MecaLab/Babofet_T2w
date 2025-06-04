3284bc081e4a7d111d2fbb241d04bdca9773532c
leroux.b <baptiste.leroux@univ-amu.fr>
2025-06-04 16:14:20 +0200

    for meso

--- nifty_module/05_image_reconstruction.py
+++ nifty_module/05_image_reconstruction.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
 
     list_subjs = [
         # "sub-Aziza_ses-01",  "sub-Aziza_ses-09", # "sub-Aziza_ses-05",
-        "sub-Borgne_ses-01", "sub-Borgne_ses-03", "sub-Borgne_ses-04",
+        "sub-Borgne_ses-05", "sub-Borgne_ses-06", "sub-Borgne_ses-07",
         # "sub-Borgne_ses-01", "sub-Borgne_ses-03", "sub-Borgne_ses-04", "sub-Borgne_ses-05", "sub-Borgne_ses-06", "sub-Borgne_ses-07"
     ]
 
