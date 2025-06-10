6964e258981b31b450f08715e8f6f96a7203eb46
leroux.b <baptiste.leroux@univ-amu.fr>
2025-06-10 10:32:02 +0200

    for meso

--- nnunet_training/01_process_label.py
+++ nnunet_training/01_process_label.py
@@ -9,35 +9,36 @@ import configuration as cfg
 
 if __name__ == "__main__":
     subj = "Fabienne"
-    session = "01"
-    bounti_srr_seg_path = os.path.join(cfg.SEG_OUTPUT_PATH, subj, f"ses{session}")
+    base_path = os.path.join(cfg.SEG_OUTPUT_PATH, subj)
+    for session in os.listdir(base_path):
+        bounti_srr_seg_path = os.path.join(base_path, f"ses{session}")
 
-    seg_img = nib.load(os.path.join(bounti_srr_seg_path, "reo-SVR-output-brain_rhesus-mask-brain_bounti-19.nii.gz"))
-    seg_mask = seg_img.get_fdata()
+        seg_img = nib.load(os.path.join(bounti_srr_seg_path, "reo-SVR-output-brain_rhesus-mask-brain_bounti-19.nii.gz"))
+        seg_mask = seg_img.get_fdata()
 
-    new_mask = np.zeros_like(seg_mask)
+        new_mask = np.zeros_like(seg_mask)
 
-    csf_labels = [1, 2, 18, 19]
-    new_mask[np.isin(seg_mask, csf_labels)] = 1
+        csf_labels = [1, 2, 18, 19]
+        new_mask[np.isin(seg_mask, csf_labels)] = 1
 
-    wm_labels = [5, 6]
-    new_mask[np.isin(seg_mask, wm_labels)] = 2
+        wm_labels = [5, 6]
+        new_mask[np.isin(seg_mask, wm_labels)] = 2
 
-    gm_labels = [3, 4]
-    new_mask[np.isin(seg_mask, gm_labels)] = 3
+        gm_labels = [3, 4]
+        new_mask[np.isin(seg_mask, gm_labels)] = 3
 
-    ventricle_labels = [7, 8, 9]
-    new_mask[np.isin(seg_mask, ventricle_labels)] = 4
+        ventricle_labels = [7, 8, 9]
+        new_mask[np.isin(seg_mask, ventricle_labels)] = 4
 
-    thalamus_labels = [16, 17]
-    new_mask[np.isin(seg_mask, thalamus_labels)] = 5
+        thalamus_labels = [16, 17]
+        new_mask[np.isin(seg_mask, thalamus_labels)] = 5
 
-    cerebellum_labels = [10, 11, 12, 13]
-    new_mask[np.isin(seg_mask, cerebellum_labels)] = 6
+        cerebellum_labels = [10, 11, 12, 13]
+        new_mask[np.isin(seg_mask, cerebellum_labels)] = 6
 
-    basal_ganglia_labels = [14, 15]
-    new_mask[np.isin(seg_mask, basal_ganglia_labels)] = 7
+        basal_ganglia_labels = [14, 15]
+        new_mask[np.isin(seg_mask, basal_ganglia_labels)] = 7
 
-    # Sauvegarder le nouveau masque
-    new_segmentation_img = nib.Nifti1Image(new_mask, seg_img.affine, seg_img.header)
-    nib.save(new_segmentation_img, os.path.join(bounti_srr_seg_path, "reo-SVR-output-brain_rhesus-mask-brain_bounti-7.nii.gz"))
+        # Sauvegarder le nouveau masque
+        new_segmentation_img = nib.Nifti1Image(new_mask, seg_img.affine, seg_img.header)
+        nib.save(new_segmentation_img, os.path.join(bounti_srr_seg_path, "reo-SVR-output-brain_rhesus-mask-brain_bounti-7.nii.gz"))
