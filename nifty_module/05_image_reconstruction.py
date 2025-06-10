8d4fd0fd9d46a48c27d9f93f20e3d00ebdf96f58
leroux.b <baptiste.leroux@univ-amu.fr>
2025-06-10 17:09:04 +0200

    for meso

--- nifty_module/05_image_reconstruction.py
+++ nifty_module/05_image_reconstruction.py
@@ -54,8 +54,8 @@ singularity exec \\
         --dir-output /output/ \\
         --isotropic-resolution 0.5 \\
         --bias-field-correction 0 \\
-        --template /template/Template_G85_T2W.nii.gz \\
-        --template-mask /template/Template_G85_T2W_mask.nii.gz \\
+        --template /template/Template_G135_T2W.nii.gz \\
+        --template-mask /template/Template_G135_T2W_mask.nii.gz \\
         
               
 """
@@ -102,10 +102,10 @@ if __name__ == "__main__":
 
     SUFFIX_EXP = ""  # need to be updated for every exp. If prexif exist, should start with _
 
-    subj = "Borgne"
     list_subjs = [
         # "sub-Aziza_ses-01",  "sub-Aziza_ses-09", # "sub-Aziza_ses-05",
-        f"sub-{subj}_ses-08",
+        "sub-Borgne_ses-08", "sub-Borgne_ses-10", "sub-Borgne_ses-09"
+        # "sub-Filoutte_ses-01", "sub-Filoutte_ses-02", "sub-Filoutte_ses-03",
         # "sub-Borgne_ses-01", "sub-Borgne_ses-03", "sub-Borgne_ses-04", "sub-Borgne_ses-05", "sub-Borgne_ses-06", "sub-Borgne_ses-07"
     ]
 
