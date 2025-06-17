5b61b5788e517d41ee727fff14c9b40e30cbdb93
leroux.b <baptiste.leroux@univ-amu.fr>
2025-06-17 09:51:40 +0200

    for meso

--- nnunet_training/01_process_label.py
+++ nnunet_training/01_process_label.py
@@ -8,36 +8,36 @@ import configuration as cfg
 
 
 if __name__ == "__main__":
-    subj = "Borgne"
+    subj = "Filoutte"
     base_path = os.path.join(cfg.SEG_OUTPUT_PATH_NIOLON, subj)
     for session in os.listdir(base_path):
         bounti_srr_seg_path = os.path.join(base_path, session)
 
-        output_path = os.path.join(bounti_srr_seg_path, "reo-SVR-output-brain_rhesus-mask-brain_bounti-7.nii.gz")
+        output_path = os.path.join(bounti_srr_seg_path, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
 
-        seg_img = nib.load(os.path.join(bounti_srr_seg_path, "reo-SVR-output-brain_rhesus-mask-brain_bounti-7.nii.gz"))
+        seg_img = nib.load(os.path.join(bounti_srr_seg_path, "reo-SVR-output-brain_rhesus-mask-brain_bounti-19.nii.gz"))
         seg_mask = seg_img.get_fdata()
 
         new_mask = np.zeros_like(seg_mask)
 
-        csf_labels = [1, 5, 6, 7]
+        csf_labels = [1, 2, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
         new_mask[np.isin(seg_mask, csf_labels)] = 1
 
-        wm_labels = [2]
+        wm_labels = [5, 6]
         new_mask[np.isin(seg_mask, wm_labels)] = 2
 
-        gm_labels = [3]
+        gm_labels = [3, 4]
         new_mask[np.isin(seg_mask, gm_labels)] = 3
 
-        ventricle_labels = [4]
+        ventricle_labels = [7, 8, 9]
         new_mask[np.isin(seg_mask, ventricle_labels)] = 4
 
         new_segmentation_img = nib.Nifti1Image(new_mask, seg_img.affine, seg_img.header)
         nib.save(new_segmentation_img, output_path)
+        print(f"OK for {subj} {session}")
 
-
-
-        """new_mask = np.zeros_like(seg_mask)
+        """
+        new_mask = np.zeros_like(seg_mask)
 
         csf_labels = [1, 2, 18, 19]
         new_mask[np.isin(seg_mask, csf_labels)] = 1
@@ -64,4 +64,5 @@ if __name__ == "__main__":
         new_segmentation_img = nib.Nifti1Image(new_mask, seg_img.affine, seg_img.header)
         nib.save(new_segmentation_img, output_path)
 
-        print(f"OK for {subj} {session}")"""
+        print(f"OK for {subj} {session}")
+        """
