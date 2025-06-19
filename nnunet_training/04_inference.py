ecece4324ac63808376673a70da27d6cc3f84807
leroux.b <baptiste.leroux@univ-amu.fr>
2025-06-19 15:14:55 +0200

    for meso

---
+++ nnunet_training/04_inference.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+
+def write_slurm_file(input_folder, output_folder):
+    filename = "slurm_files/nnunet_prediction.slurm"
+    slurm_content = f"""#!/bin/bash
+
+
+#SBATCH --account='b219'
+#SBATCH --partition=volta
+#SBATCH --gres=gpu:1
+#SBATCH --time=6:00:00
+#SBATCH -c 12
+#SBATCH -o nnunet_predict.out
+#SBATCH -e nnunet_predict.err
+
+module purge
+module load userspace/all
+module load gcc/14.1.0
+module load cuda/12.4
+
+source ~/.bashrc
+conda activate nnunet
+
+nnUNetv2_predict -i {input_folder} -o {output_folder} -d 1 -c 3d_fullres -f 1
+
+"""
+    with open(filename, "w", encoding="utf-8") as slurm_file:
+        slurm_file.write(slurm_content)
+
+    os.chmod(filename, 0o700)
+
+
+if __name__ == "__main__":
+    input_folder = "/scratch/lbaptiste/data/nnUNet_raw/Dataset001_Babofet/"
+    output_folder = "/scratch/lbaptiste/Babofet_T2w/snapshots/nnunet_res/"
+
+    print("Starting inference")
+    write_slurm_file(input_folder, output_folder)
+    subprocess.run(["sbatch", "slurm_files/nnunet_prediction.slurm"])
