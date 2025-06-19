96271adbb40f52e049603e7141934111beff5859
leroux.b <baptiste.leroux@univ-amu.fr>
2025-06-19 13:24:23 +0200

    for meso

---
+++ nnunet_training/02_prepare_dataset.py
@@ -0,0 +1,42 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
+import shutil
+
+
+if __name__ == "__main__":
+    subject_sessions = {
+        "Filoutte": ["ses03", "ses04", "ses05"],
+        # "Fabienne": ["ses03", "ses04", "ses05"],
+        # "Formule": ["ses02", "ses03"],
+        # Borgne soon
+    }
+
+    for subject, sessions in subject_sessions.items():
+
+        input_path_3d_stacks = os.path.join(cfg.SEG_INPUT_PATH_NIOLON, subject)
+        input_path_3d_segs = os.path.join(cfg.SEG_OUTPUT_PATH_NIOLON, subject)
+
+        output_path = cfg.NNUNET_RAW_PATH_NIOLON
+
+        images_tr_path = os.path.join(output_path, "ImagesTr")
+        images_ts_path = os.path.join(output_path, "ImagesTs")
+        labels_tr_path = os.path.join(output_path, "LabelsTr")
+
+        if not os.path.exists(images_tr_path):
+            os.makedirs(images_tr_path)
+        if not os.path.exists(images_ts_path):
+            os.makedirs(images_ts_path)
+        if not os.path.exists(labels_tr_path):
+            os.makedirs(labels_tr_path)
+
+        for session in sessions:
+            input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "reo-SVR-output-brain_rhesus.nii.gz")
+            input_path_3d_seg = os.path.join(input_path_3d_segs, session, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
+
+            output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_0000.nii.gz")
+            output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}.nii.gz")
+
+            shutil.copy2(input_path_3d_stack, output_path_3d_stack)
+            shutil.copy2(input_path_3d_seg, output_path_3d_seg)
