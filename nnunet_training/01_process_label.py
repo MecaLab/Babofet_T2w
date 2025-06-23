e4317ea8eae27ec9e7fff7f0257d1f8095a51b4f
leroux.b <baptiste.leroux@univ-amu.fr>
2025-06-23 13:16:51 +0200

    for meso

--- nnunet_training/01_process_label.py
+++ nnunet_training/01_process_label.py
@@ -35,34 +35,3 @@ if __name__ == "__main__":
         new_segmentation_img = nib.Nifti1Image(new_mask, seg_img.affine, seg_img.header)
         nib.save(new_segmentation_img, output_path)
         print(f"OK for {subj} {session}")
-
-        """
-        new_mask = np.zeros_like(seg_mask)
-
-        csf_labels = [1, 2, 18, 19]
-        new_mask[np.isin(seg_mask, csf_labels)] = 1
-
-        wm_labels = [5, 6]
-        new_mask[np.isin(seg_mask, wm_labels)] = 2
-
-        gm_labels = [3, 4]
-        new_mask[np.isin(seg_mask, gm_labels)] = 3
-
-        ventricle_labels = [7, 8, 9]
-        new_mask[np.isin(seg_mask, ventricle_labels)] = 4
-
-        thalamus_labels = [16, 17]
-        new_mask[np.isin(seg_mask, thalamus_labels)] = 5
-
-        cerebellum_labels = [10, 11, 12, 13]
-        new_mask[np.isin(seg_mask, cerebellum_labels)] = 6
-
-        basal_ganglia_labels = [14, 15]
-        new_mask[np.isin(seg_mask, basal_ganglia_labels)] = 7
-
-        # Sauvegarder le nouveau masque
-        new_segmentation_img = nib.Nifti1Image(new_mask, seg_img.affine, seg_img.header)
-        nib.save(new_segmentation_img, output_path)
-
-        print(f"OK for {subj} {session}")
-        """
