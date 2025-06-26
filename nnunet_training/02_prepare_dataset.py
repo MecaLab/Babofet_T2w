cb41b0270dabbff630fa38d515441fed7f499b05
leroux.b <baptiste.leroux@univ-amu.fr>
2025-06-26 13:19:57 +0200

    for meso

--- nnunet_training/02_prepare_dataset.py
+++ nnunet_training/02_prepare_dataset.py
@@ -3,41 +3,50 @@ import sys
 sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 import shutil
-import nibabel as nib
-import numpy as np
 
 
 if __name__ == "__main__":
     subject_sessions = {
         "Borgne": ["ses08"],
-        "Fabienne": ["ses08"],
-        # Borgne soon
+        "Fabienne": ["ses03", "ses04", "ses05", "ses08"],
+        "Filoutte": ["ses02", "ses03"],
+        "Formule": ["ses03", "ses04", "ses05"],
     }
 
-    for subject, sessions in subject_sessions.items():
+    crop_data = True
+    dataset_name = "Dataset003_BabofetUncropped"
 
-        input_path_3d_stacks = os.path.join(cfg.BOUNTI_PATH, "svrtk_BOUNTI/input_SRR_niftymic/haste", subject)
+    output_path = os.path.join(cfg.NNUNET_RAW_PATH, dataset_name)
+
+    images_tr_path = os.path.join(output_path, "imagesTr")
+    images_ts_path = os.path.join(output_path, "imagesTs")
+    labels_tr_path = os.path.join(output_path, "labelsTr")
+
+    if not os.path.exists(images_tr_path):
+        os.makedirs(images_tr_path)
+    if not os.path.exists(images_ts_path):
+        os.makedirs(images_ts_path)
+    if not os.path.exists(labels_tr_path):
+        os.makedirs(labels_tr_path)
+
+    for subject, sessions in subject_sessions.items():
         input_path_3d_segs = os.path.join(cfg.BOUNTI_PATH, "svrtk_BOUNTI/output_BOUNTI_seg/haste", subject)
 
-        output_path = os.path.join(cfg.NNUNET_RAW_PATH, "Dataset001_BabofetFineTuning")
+        if crop_data:
+            input_path_3d_stacks = os.path.join(cfg.BOUNTI_PATH, "svrtk_BOUNTI/input_SRR_niftymic/haste", subject)
+        else:
+            input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
 
-        images_tr_path = os.path.join(output_path, "imagesTr")
-        images_ts_path = os.path.join(output_path, "imagesTs")
-        labels_tr_path = os.path.join(output_path, "labelsTr")
+            for session in sessions:
+                if crop_data:
+                    input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "reo-SVR-output-brain_rhesus.nii.gz")
+                else:
+                    input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
 
-        if not os.path.exists(images_tr_path):
-            os.makedirs(images_tr_path)
-        if not os.path.exists(images_ts_path):
-            os.makedirs(images_ts_path)
-        if not os.path.exists(labels_tr_path):
-            os.makedirs(labels_tr_path)
+                input_path_3d_seg = os.path.join(input_path_3d_segs, session, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
 
-        for session in sessions:
-            input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "reo-SVR-output-brain_rhesus.nii.gz")
-            input_path_3d_seg = os.path.join(input_path_3d_segs, session, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
+                output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_0000.nii.gz")
+                output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}.nii.gz")
 
-            output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_0000.nii.gz")
-            output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}.nii.gz")
-
-            shutil.copy2(input_path_3d_stack, output_path_3d_stack)
-            shutil.copy2(input_path_3d_seg, output_path_3d_seg)
+                shutil.copy2(input_path_3d_stack, output_path_3d_stack)
+                shutil.copy2(input_path_3d_seg, output_path_3d_seg)
