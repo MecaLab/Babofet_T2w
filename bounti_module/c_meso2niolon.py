1ad737ccea0ad5043e49dd47fd294d0a65bff7e9
leroux.b <baptiste.leroux@univ-amu.fr>
2025-07-02 09:22:43 +0200

    for meso

--- bounti_module/c_meso2niolon.py
+++ bounti_module/c_meso2niolon.py
@@ -6,17 +6,11 @@ import configuration as cfg
 
 if __name__ == "__main__":
 
-    subject = sys.argv[1]
+    subject = sys.argv[1]  # subject name, e.g., Fabienne / Formule / etc..
     user_id = "lbaptiste"
 
     base_path = os.path.join(cfg.DATA_PATH, subject)
 
     command = f"scp -P 8822 -r {user_id}@login.mesocentre.univ-amu.fr:{base_path} {cfg.RECONS_FOLDER}"
 
-    print(command)
-
     subprocess.run(command, shell=True)
-
-    """
-    scp -P 8822 lbaptiste@login.mesocentre.univ-amu.fr:base_path/ recons_folder
-    """
