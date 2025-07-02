a56bf8ae3b4249daeadb3a933093e2873f7abbc7
leroux.b <baptiste.leroux@univ-amu.fr>
2025-07-02 14:54:02 +0200

    for meso

--- nnunet_module/02_prepare_dataset.py
+++ nnunet_module/02_prepare_dataset.py
@@ -62,25 +62,25 @@ if __name__ == "__main__":
         print(f"Processing subject: {subject}")
         input_path_3d_segs = os.path.join(cfg.BOUNTI_PATH, "svrtk_BOUNTI/output_BOUNTI_seg/haste", subject)
 
-        if not crop_data:
-            input_path_3d_stacks = os.path.join(cfg.BOUNTI_PATH, "svrtk_BOUNTI/input_SRR_niftymic/haste", subject)
-        else:
+        if crop_data:
             input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
+        else:
+            input_path_3d_stacks = os.path.join(cfg.BOUNTI_PATH, "svrtk_BOUNTI/input_SRR_niftymic/haste", subject)
 
-            for session in sessions:
-                print(f"\tProcessing session: {session}")
-                if not crop_data:
-                    input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "reo-SVR-output-brain_rhesus.nii.gz")
-                else:
-                    input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
+        for session in sessions:
+            print(f"\tProcessing session: {session}")
+            if crop_data:
+                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
+            else:
+                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "reo-SVR-output-brain_rhesus.nii.gz")
 
-                input_path_3d_seg = os.path.join(input_path_3d_segs, session, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
+            input_path_3d_seg = os.path.join(input_path_3d_segs, session, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
 
-                output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_0000.nii.gz")
-                output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}.nii.gz")
+            output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_0000.nii.gz")
+            output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}.nii.gz")
 
-                shutil.copy2(input_path_3d_stack, output_path_3d_stack)
-                shutil.copy2(input_path_3d_seg, output_path_3d_seg)
+            shutil.copy2(input_path_3d_stack, output_path_3d_stack)
+            shutil.copy2(input_path_3d_seg, output_path_3d_seg)
 
     dataset_json = os.path.join(output_path, "dataset.json")
     num_training = len(os.listdir(images_tr_path))
