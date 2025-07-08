f4a44ccc8e173494f66aed5ed655ef4bb31ce0dd
leroux.b <baptiste.leroux@univ-amu.fr>
2025-07-08 15:55:06 +0200

    for meso

---
+++ nnunet_module/08_proba.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+import sys
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
+
+
+if __name__ == "__main__":
+
+    dataset_id = 3
+    output_folder = f"/scratch/lbaptiste/Babofet_T2w/snapshots/nnunet_res/pred_dataset_{dataset_id}"
+
+    for file in os.listdir(output_folder):
+        if file.endswith(".nii.gz"):
+            seg_path = os.path.join(output_folder, file)
+            npz_path = os.path.join(output_folder, file.replace(".nii.gz", ".npz"))
+
+            npz_data = np.load(npz_path)
+
+            break
+
+
+
+
