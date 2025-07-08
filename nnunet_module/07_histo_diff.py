5cace05e8553c7c0978ff12ed4170732a7848ea9
leroux.b <baptiste.leroux@univ-amu.fr>
2025-07-08 15:04:04 +0200

    for meso

--- nnunet_module/07_histo_diff.py
+++ nnunet_module/07_histo_diff.py
@@ -3,13 +3,13 @@ import sys
 import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 
-
 if __name__ == "__main__":
-
 
     path_1 = "/scratch/lbaptiste/Babofet_T2w/snapshots/nnunet_res/pred_dataset_3"
     path_2 = "/scratch/lbaptiste/Babofet_T2w/snapshots/nnunet_res/pred_dataset_4"
@@ -19,22 +19,35 @@ if __name__ == "__main__":
     for file_1, file_2 in zip(os.listdir(path_1), os.listdir(path_2)):
         if file_1.endswith(".nii.gz") and file_2.endswith(".nii.gz"):
 
-            seg_1 = nib.load(os.path.join(path_1, file_1)).get_fdata()
-            seg_2 = nib.load(os.path.join(path_2, file_2)).get_fdata()
+            mask1 = nib.load(os.path.join(path_1, file_1)).get_fdata()
+            mask2 = nib.load(os.path.join(path_2, file_2)).get_fdata()
 
-            diff = seg_1 - seg_2
+            mask1_fg = mask1[mask1 > 0]
+            mask2_fg = mask2[mask1 > 0]
 
-            plt.figure(figsize=(10, 5))
-            plt.hist(diff.flatten(), bins=np.arange(diff.min(), diff.max() + 2) - 0.5, color='skyblue',
-                     edgecolor='black')
-            plt.title("Histogramme des différences entre les masques")
-            plt.xlabel("Valeur de (mask1 - mask2)")
+            diff = mask1_fg - mask2_fg
+
+            plt.figure(figsize=(8, 4))
+            plt.hist(diff, bins=np.arange(diff.min(), diff.max() + 2) - 0.5, color='lightcoral', edgecolor='black')
+            plt.title("Histogramme des différences (mask1 - mask2)")
+            plt.xlabel("Différences de labels (mask1 - mask2)")
             plt.ylabel("Nombre de voxels")
             plt.grid(True)
             plt.xticks(np.arange(diff.min(), diff.max() + 1))
-            plt.tight_layout()
-
             output_fig = os.path.join(cfg.BASE_PATH, f"Babofet_T2w/snapshots/nnunet_res/histo_diff_{file_1}.png")
+            plt.tight_layout()
+            plt.savefig(output_fig)
+
+            # 2. Matrice de confusion entre les deux masques (hors background)
+            cm = confusion_matrix(mask1_fg, mask2_fg, labels=[1, 2, 3, 4])
+
+            plt.figure(figsize=(6, 5))
+            sns.heatmap(cm, annot=True, fmt='d', cmap="Blues", xticklabels=[1, 2, 3, 4], yticklabels=[1, 2, 3, 4])
+            plt.xlabel("Prédiction modèle 2")
+            plt.ylabel("Prédiction modèle 1")
+            plt.title("Matrice de confusion entre masques (sans background)")
+            output_fig = os.path.join(cfg.BASE_PATH, f"Babofet_T2w/snapshots/nnunet_res/heatmap_diff_{file_1}.png")
+            plt.tight_layout()
             plt.savefig(output_fig)
             plt.close()
             break
