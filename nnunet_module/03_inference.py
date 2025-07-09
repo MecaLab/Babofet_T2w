e8289d0c9fed6e52f67a5fd0fd0c06f6ea2dda67
leroux.b <baptiste.leroux@univ-amu.fr>
2025-07-09 11:24:38 +0200

    for meso

--- nnunet_module/03_inference.py
+++ nnunet_module/03_inference.py
@@ -45,10 +45,10 @@ if __name__ == "__main__":
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-
     subject_sessions = {
         "Bibi": ["ses04", "ses05", "ses06", "ses07", "ses09"],
         "Borgne": ["ses04", "ses05", "ses06", "ses07", "ses09", "ses10"],
+        "Filoutte": ["ses06", "ses07", "ses08", "ses09", "ses10"]
         # "Aziza": ["ses02", "ses03", "ses04", "ses05", "ses06", "ses07", "ses08", "ses09", "ses10"],
         # "Forme": ["ses02", "ses03", "ses05", "ses06", "ses07", "ses08", "ses09", "ses10"],
     }
