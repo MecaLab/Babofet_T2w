7fb90bf92254f639e955025a521f82e88f9cd642
leroux.b <baptiste.leroux@univ-amu.fr>
2025-07-30 15:28:49 +0200

    for meso

--- longiseg_module/01_prepare_dataset.py
+++ longiseg_module/01_prepare_dataset.py
@@ -108,10 +108,9 @@ if __name__ == "__main__":
 
             # shutil.copy(input_path_3d_stack, output_path_3d_stack)
             os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
-            exit()
             # shutil.copy2(input_path_3d_stack_bis, output_path_3d_stack_bis)
 
-            shutil.copy(input_path_3d_seg, output_path_3d_seg)
+            os.system(f"cp {input_path_3d_seg} {output_path_3d_seg}")
             # shutil.copy2(input_path_3d_seg, output_path_3d_seg_bis)
 
             # Previous sess
@@ -127,7 +126,7 @@ if __name__ == "__main__":
             output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{previous_sess}_bias_0000.nii.gz")
             # output_path_3d_stack_bis = os.path.join(images_tr_path, f"{subject}_{previous_sess}_debias_t{previous_sess[3:]}_0000.nii.gz")
 
-            shutil.copy(input_path_3d_stack, output_path_3d_stack)
+            os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
             # shutil.copy2(input_path_3d_stack_bis, output_path_3d_stack_bis)
 
     dataset_json = os.path.join(output_path, "dataset.json")
