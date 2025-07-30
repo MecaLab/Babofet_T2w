b60f17b0395395ced1f5e6f6ae623bd5ab7dcf9e
leroux.b <baptiste.leroux@univ-amu.fr>
2025-07-30 15:50:15 +0200

    for meso

--- longiseg_module/01_prepare_dataset.py
+++ longiseg_module/01_prepare_dataset.py
@@ -122,7 +122,11 @@ if __name__ == "__main__":
             output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{previous_sess}_bias_0000.nii.gz")
             # output_path_3d_stack_bis = os.path.join(images_tr_path, f"{subject}_{previous_sess}_debias_t{previous_sess[3:]}_0000.nii.gz")
 
+            input_path_3d_seg = os.path.join(previous_path_3d_seg, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
+            output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}_bias.nii.gz")
+
             os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
+            os.system(f"cp {input_path_3d_seg} {output_path_3d_seg}")
             # shutil.copy2(input_path_3d_stack_bis, output_path_3d_stack_bis)
 
     dataset_json = os.path.join(output_path, "dataset.json")
