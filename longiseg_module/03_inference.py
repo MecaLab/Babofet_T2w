77df43645de1d1ebb9bfb8510a055b7c1fb4371f
leroux.b <baptiste.leroux@univ-amu.fr>
2025-08-11 09:41:46 +0200

    for meso

--- longiseg_module/03_inference.py
+++ longiseg_module/03_inference.py
@@ -6,10 +6,8 @@ sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 
 
-def write_slurm_file(input_folder, output_folder, dataset_id, trainer):
-    filename = "slurm_files/longiseg_prediction.slurm"
+def write_slurm_file(input_folder, output_folder, filename, dataset_id, trainer):
     slurm_content = f"""#!/bin/bash
-
 
 #SBATCH --account='b219'
 #SBATCH --partition=volta
@@ -55,44 +53,8 @@ if __name__ == "__main__":
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Mettre dans ça dans 01_prepare_dataset dans le dossiers imagesTs (voir doc)
-
-    test_subject_sessions = {
-        "Bibi": ["ses04", "ses05", "ses06", "ses09"],
-        "Borgne": ["ses04", "ses05", "ses06", "ses10"],
-        "Filoutte": ["ses06", "ses07", "ses09", "ses10"],
-        "Fabienne": ["ses07", "ses09"],
-        # "Aziza": ["ses02", "ses03", "ses04", "ses05", "ses06", "ses07", "ses08", "ses09", "ses10"],
-        # "Forme": ["ses02", "ses03", "ses05", "ses06", "ses07", "ses08", "ses09", "ses10"],
-    }
-
-    mode_dataset = "debiased-2"  # "masked" or "unmasked" or "debiased-2"
-
-    for subject, sessions in test_subject_sessions.items():
-        print(f"Processing subject: {subject}")
-        if mode_dataset == "unmasked" or mode_dataset == "debiased-2":
-            input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
-        elif mode_dataset == "masked":
-            input_path_3d_stacks = os.path.join(cfg.BOUNTI_PATH, "svrtk_BOUNTI/input_SRR_niftymic/haste", subject)
-        else:
-            raise ValueError(f"Unknown mode_dataset: {mode_dataset}")
-
-        for session in sessions:
-            print(f"\tProcessing session: {session}")
-            if mode_dataset == "unmasked" or mode_dataset == "debiased-2":
-                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
-            elif mode_dataset == "masked":
-                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "reo-SVR-output-brain_rhesus.nii.gz")
-            else:
-                raise ValueError(f"Unknown mode_dataset: {mode_dataset}")
-
-            output_path_3d_stack = os.path.join(input_folder, f"{subject}_{session}_0000.nii.gz")
-            if os.path.exists(output_path_3d_stack):
-                print(f"\t\tOutput file {output_path_3d_stack} already exists, skipping copy.")
-                continue
-            shutil.copy2(input_path_3d_stack, output_path_3d_stack)
-
     print("Starting inference")
 
-    write_slurm_file(input_folder, output_folder, dataset_id, trainer)
+    filename = "slurm_files/longiseg_prediction.slurm"
+    write_slurm_file(input_folder, output_folder, filename, dataset_id, trainer)
     subprocess.run(["sbatch", "slurm_files/longiseg_prediction.slurm"])
