205e4dbbb1f2cc2f482d53211a04d804052738d8
leroux.b <baptiste.leroux@univ-amu.fr>
2025-08-11 10:17:59 +0200

    for meso

--- longiseg_module/01_prepare_dataset.py
+++ longiseg_module/01_prepare_dataset.py
@@ -63,8 +63,6 @@ if __name__ == "__main__":
         # "Forme": ["ses02", "ses03", "ses05", "ses06", "ses07", "ses08", "ses09", "ses10"],
     }
 
-    mode_dataset = "debiased-2"  # "masked" or "unmasked" or "debiased-2" or "full"
-
     id_dataset = int(sys.argv[1])  # should be integer, eg, 1, 2, 3, etc.
     name = sys.argv[2]  # the dataset name, can be whatever you want, but you will need to use it later so remember it
     if id_dataset < 10:
@@ -93,84 +91,71 @@ if __name__ == "__main__":
         print(f"\tProcessing subject: {subject}")
         input_path_3d_segs = os.path.join(cfg.SEG_OUTPUT_PATH, subject)
 
-        if mode_dataset == "unmasked":
-            input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
-        elif mode_dataset == "masked":
-            input_path_3d_stacks = os.path.join(cfg.SEG_INPUT_PATH, subject)
-        elif mode_dataset == "debiased-2":
-            input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
-        else:
-            raise ValueError(f"Unknown mode_dataset: {mode_dataset}")
+        input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
 
         for session in sessions:
             print(f"\t\tProcessing session: {session}")
-            if mode_dataset == "unmasked":
-                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
-            elif mode_dataset == "masked":
-                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "reo-SVR-output-brain_rhesus.nii.gz")
-            elif mode_dataset == "debiased-2":
-                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template.nii.gz")
-                input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
+
+            # Current session
+            input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template.nii.gz")
+            input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
 
             input_path_3d_seg = os.path.join(input_path_3d_segs, session, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
 
-            if mode_dataset == "debiased-2":
-                output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_bias_0000.nii.gz")
-                output_path_3d_stack_bis = os.path.join(images_tr_path, f"{subject}_{session}_debias_0000.nii.gz")
+            output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_bias_0000.nii.gz")
+            output_path_3d_stack_bis = os.path.join(images_tr_path, f"{subject}_{session}_debias_0000.nii.gz")
 
-                output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}_bias.nii.gz")
-                output_path_3d_seg_bis = os.path.join(labels_tr_path, f"{subject}_{session}_debias.nii.gz")
+            output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}_bias.nii.gz")
+            output_path_3d_seg_bis = os.path.join(labels_tr_path, f"{subject}_{session}_debias.nii.gz")
 
-                os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
-                os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
+            os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
+            os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
 
-                os.system(f"cp {input_path_3d_seg} {output_path_3d_seg}")
-                os.system(f"cp {input_path_3d_seg} {output_path_3d_seg_bis}")
+            os.system(f"cp {input_path_3d_seg} {output_path_3d_seg}")
+            os.system(f"cp {input_path_3d_seg} {output_path_3d_seg_bis}")
 
-            else:
-                output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_0000.nii.gz")
-                output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}.nii.gz")
+            # Previous session
+            previous_sess = get_previous_session_number(session)
+            print(f"\t\t\tProcessing previous session: {previous_sess}")
 
-                os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
-                os.system(f"cp {input_path_3d_seg} {output_path_3d_seg}")
+            previous_path_3d_stack = os.path.join(input_path_3d_stacks, previous_sess)
+            previous_path_3d_seg = os.path.join(input_path_3d_segs, previous_sess)
+
+            input_path_3d_stack = os.path.join(previous_path_3d_stack, "recons_rhesus/recon_template_space/srr_template.nii.gz")
+            input_path_3d_stack_bis = os.path.join(previous_path_3d_stack, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
+
+            output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{previous_sess}_bias_0000.nii.gz")
+            output_path_3d_stack_bis = os.path.join(images_tr_path, f"{subject}_{previous_sess}_debias_0000.nii.gz")
+
+            os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
+            os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
+
+            input_path_3d_seg = os.path.join(previous_path_3d_seg, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
+            output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{previous_sess}_bias.nii.gz")
+            output_path_3d_seg_bis = os.path.join(labels_tr_path, f"{subject}_{previous_sess}_debias.nii.gz")
+
+            os.system(f"cp {input_path_3d_seg} {output_path_3d_seg}")
+            os.system(f"cp {input_path_3d_seg} {output_path_3d_seg_bis}")
 
     print("Test processing...")
     # test processing
     for subject, sessions in test_subject_sessions.items():
         print(f"\tProcessing subject: {subject}")
         input_path_3d_segs = os.path.join(cfg.SEG_OUTPUT_PATH, subject)
-
-        if mode_dataset == "unmasked":
-            input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
-        elif mode_dataset == "masked":
-            input_path_3d_stacks = os.path.join(cfg.SEG_INPUT_PATH, subject)
-        elif mode_dataset == "debiased-2":
-            input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
-        else:
-            raise ValueError(f"Unknown mode_dataset: {mode_dataset}")
+        input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
 
         for session in sessions:
             print(f"\t\tProcessing session: {session}")
-            if mode_dataset == "unmasked":
-                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
-            elif mode_dataset == "masked":
-                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "reo-SVR-output-brain_rhesus.nii.gz")
-            elif mode_dataset == "debiased-2":
-                input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template.nii.gz")
-                input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
+            input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template.nii.gz")
+            input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
 
             input_path_3d_seg = os.path.join(input_path_3d_segs, session, "reo-SVR-output-brain_rhesus-mask-brain_bounti-4.nii.gz")
 
-            if mode_dataset == "debiased-2":
-                output_path_3d_stack = os.path.join(images_ts_path, f"{subject}_{session}_bias_0000.nii.gz")
-                output_path_3d_stack_bis = os.path.join(images_ts_path, f"{subject}_{session}_debias_0000.nii.gz")
+            output_path_3d_stack = os.path.join(images_ts_path, f"{subject}_{session}_bias_0000.nii.gz")
+            output_path_3d_stack_bis = os.path.join(images_ts_path, f"{subject}_{session}_debias_0000.nii.gz")
 
-                os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
-                os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
-
-            else:
-                output_path_3d_stack = os.path.join(images_ts_path, f"{subject}_{session}_0000.nii.gz")
-                os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
+            os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
+            os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
 
     dataset_json = os.path.join(output_path, "dataset.json")
     num_training = len(os.listdir(labels_tr_path))
