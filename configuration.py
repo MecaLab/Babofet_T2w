c3eae5d2933b47992d5aa2cdf477df0eabd594b0
leroux.b <baptiste.leroux@univ-amu.fr>
2025-08-12 10:32:04 +0200

    for meso

--- configuration.py
+++ configuration.py
@@ -6,40 +6,37 @@ import os
 # REPOSITORY DATA ORGANISATION
 # -----------------------------------------------------------------------------/
 
-# LOCAL PATH
-MAIN_PATH = "W:/meca/data/Fetus/datasets/Babofet"
-OUTPUT_PATH = os.path.join(MAIN_PATH, "processing")
-
 # NIOLON PATH
 BASE_NIOLON_PATH = "/envau/work/meca/data/babofet_DB/2024_new_stuff"
 RECONS_FOLDER = os.path.join(BASE_NIOLON_PATH, "recons_folder")
-BOUNTI_NIOLON_PATH = os.path.join(BASE_NIOLON_PATH, "bounti")
-SEG_INPUT_PATH_NIOLON = os.path.join(BOUNTI_NIOLON_PATH, "svrtk_BOUNTI", "input_SRR_niftymic", "haste")
-SEG_OUTPUT_PATH_NIOLON = os.path.join(BOUNTI_NIOLON_PATH, "svrtk_BOUNTI", "output_BOUNTI_seg", "haste")
+BOUNTI_NIOLON_PATH = os.path.join(BASE_NIOLON_PATH, "bounti", "svrtk_BOUNTI")
+SEG_INPUT_PATH_NIOLON = os.path.join(BOUNTI_NIOLON_PATH, "input_SRR_niftymic", "haste")
+SEG_OUTPUT_PATH_NIOLON = os.path.join(BOUNTI_NIOLON_PATH, "output_BOUNTI_seg", "haste")
 
 
 # MESOCENTRE PATH
-BASE_PATH = "/scratch/lbaptiste/"
-MESO_DATA_PATH = os.path.join(BASE_PATH, "data/dataset/babofet/subjects")
-MESO_OUTPUT_PATH = os.path.join(BASE_PATH, "data/dataset/babofet/derivatives")
+BASE_PATH = "/scratch/lbaptiste/data"
+CODE_PATH = "/scratch/lbaptiste/Babofet_T2w/"
+MESO_DATA_PATH = os.path.join(BASE_PATH, "dataset/babofet/subjects")
+MESO_OUTPUT_PATH = os.path.join(BASE_PATH, "dataset/babofet/derivatives")
 # DATA PATH
-DATA_PATH = os.path.join(BASE_PATH, "data/recons_folder")
-BOUNTI_PATH = os.path.join(BASE_PATH, "data/bounti")
-ATLAS_GHOLIPOUR_PATH = os.path.join(BASE_PATH, "data/STA_atlas_hemi_split")
+DATA_PATH = os.path.join(BASE_PATH, "recons_folder")
+ATLAS_GHOLIPOUR_PATH = os.path.join(BASE_PATH, "STA_atlas_hemi_split")
 
 # nnUNet path
-NNUNET_RAW_PATH = os.path.join(BASE_PATH, "data/nnUNet_raw")
-NNUNET_RESULTS_PATH = os.path.join(BASE_PATH, "data/nnUNet_trained_models")
-NNUNET_PREPROCESSED_PATH = os.path.join(BASE_PATH, "data/nnUNet_preprocessed")
+NNUNET_RAW_PATH = os.path.join(BASE_PATH, "nnUNet_raw")
+NNUNET_RESULTS_PATH = os.path.join(BASE_PATH, "nnUNet_trained_models")
+NNUNET_PREPROCESSED_PATH = os.path.join(BASE_PATH, "nnUNet_preprocessed")
 
 # LongiSeg path
-LONGISEG_RAW_PATH = os.path.join(BASE_PATH, "data/LongiSeg_raw")
-LONGISEG_RESULTS_PATH = os.path.join(BASE_PATH, "data/LongiSeg_results")
-LONGISEG_PREPROCESSED_PATH = os.path.join(BASE_PATH, "data/LongiSeg_preprocessed")
+LONGISEG_RAW_PATH = os.path.join(BASE_PATH, "LongiSeg_raw")
+LONGISEG_RESULTS_PATH = os.path.join(BASE_PATH, "LongiSeg_results")
+LONGISEG_PREPROCESSED_PATH = os.path.join(BASE_PATH, "LongiSeg_preprocessed")
 
 # Segmentation path bounti
-SEG_INPUT_PATH = os.path.join(BOUNTI_PATH, "svrtk_BOUNTI", "input_SRR_niftymic", "haste")
-SEG_OUTPUT_PATH = os.path.join(BOUNTI_PATH, "svrtk_BOUNTI", "output_BOUNTI_seg", "haste")
+BOUNTI_PATH = os.path.join(BASE_PATH, "bounti", "svrtk_BOUNTI")
+SEG_INPUT_PATH = os.path.join(BOUNTI_PATH, "input_SRR_niftymic", "haste")
+SEG_OUTPUT_PATH = os.path.join(BOUNTI_PATH, "output_BOUNTI_seg", "haste")
 
 TABLE_CSV_DATA_INFO = os.path.join("table_data", "info_table.csv")
 
