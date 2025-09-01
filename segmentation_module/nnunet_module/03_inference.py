c7a7362da5652af7452aa8a6ba6da2f0e2f101eb
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-01 13:51:35 +0200

    for meso

--- segmentation_module/nnunet_module/03_inference.py
+++ segmentation_module/nnunet_module/03_inference.py
@@ -45,12 +45,8 @@ if __name__ == "__main__":
     else:
         dataset_name = f"Dataset{dataset_id}_{name}"
 
-    # input_folder = os.path.join(cfg.NNUNET_RAW_PATH, dataset_name, "imagesTs")
-
-    input_folder = "pred_borgne_olivier_input"
-
-    # output_folder = os.path.join(cfg.CODE_PATH, f"snapshots/nnunet_res/pred_dataset_{dataset_id}")
-    output_folder = "pred_borgne_olivier_res_output"
+    input_folder = os.path.join(cfg.NNUNET_RAW_PATH, dataset_name, "imagesTs")
+    output_folder = os.path.join(cfg.CODE_PATH, f"snapshots/nnunet_res/pred_dataset_{dataset_id}")
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
