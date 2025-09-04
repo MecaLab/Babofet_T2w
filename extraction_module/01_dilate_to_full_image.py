7b191a66b58ec718b853b64bdf57cd78b4c6715c
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-04 15:37:05 +0200

    for niolon

--- extraction_module/01_dilate_to_full_image.py
+++ extraction_module/01_dilate_to_full_image.py
@@ -37,7 +37,9 @@ if __name__ == "__main__":
         filename_out = f"STA{ts}_all_reg_LR_dilall.nii.gz"
         filename_path_out = os.path.join(atlas_path, filename_out)
 
-        command = f"fslmaths {filename_path_in} -dilM -dilM -dilM -dilM {filename_path_out}"
+        print("Computing ", filename_in)
+
+        command = f"fslmaths {filename_path_in} -dilM -dilM -dilM -dilM -dilall {filename_path_out}"
         subprocess.run(command, shell=True)
 
         command = f"fslmaths {filename_path_out} -uthr 1 {filename_path_out}"
