34302646043ef4ac8a9954ab32b18a26736eff6c
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-04 11:40:38 +0200

    for niolon

--- extraction_module/01_dilate_to_full_image.py
+++ extraction_module/01_dilate_to_full_image.py
@@ -11,8 +11,7 @@ path_out = os.path.join(path_in, "tmp")
 if not os.path.exists(path_out):
     os.makedirs(path_out)
 
-for file in path_in:
-    print(file)
+for file in os.listdir(path_in):
     if file.endswith(".nii.gz"):
         print(f"Processing {file}")
 
