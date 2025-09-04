7752e1005e55d8a23207c32b384720ccb9c0b202
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-04 10:02:04 +0200

    for niolon

---
+++ extraction_module/01_dilate_to_full_image.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import sys
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
+
+
+path_in = "/envau/work/meca/data/babofet_DB/2024_new_stuff/nnunet_pred_dataset_7_3000/"
+path_out = os.path.join(path_in, "tmp")
+
+for file in path_in:
+    if file.endswith(".nii.gz"):
+        print(f"Processing {file}")
+
+        full_path_in = os.path.join(path_in, file)
+        full_path_out = os.path.join(path_out, f"dil_{file}")
+
+        command = f"fslmaths {full_path_in} -dilM -dilM -dilM -dilM -dilall {full_path_out}"
+        subprocess.run(command, shell=True)
+
+        command = f"fslmaths {full_path_out} -uthr 1 {full_path_out}"
+        subprocess.run(command, shell=True)
+
+        command = f"fslmaths {full_path_out} -dilM {full_path_out}"
+        subprocess.run(command, shell=True)
+
+        command = f"fslmaths {full_path_out} -ero {full_path_out}"
+        subprocess.run(command, shell=True)
+
+        print("Fin")
+        exit()
