25ad239c244123b7cf06c44b7749650afffe00a2
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-08 13:53:28 +0200

    for niolon

--- extraction_module/02_hemi_split.py
+++ extraction_module/02_hemi_split.py
@@ -26,62 +26,23 @@ def ants_register(fixed, altas_days):
 
 if __name__ == "__main__":
 
-    volume_dir = os.path.join(cfg.BASE_NIOLON_PATH, "recons_folder")
+    recons_folder = cfg.RECONS_FOLDER
 
-    seg_dir_in = os.path.join(cfg.BASE_NIOLON_PATH, "nnunet_pred_dataset_7_3000")
-    seg_dir_out = os.path.join(seg_dir_in, "hemi_split")
+    for subject in os.listdir(recons_folder):
+        print(f"Starting {subject}")
 
-    if not os.path.exists(seg_dir_out):
-        os.makedirs(seg_dir_out)
+        subject_path = os.path.join(recons_folder, subject)
+        for session in os.listdir(subject_path):
+            print(f"\tSession: {session}")
 
-    for file in os.listdir(seg_dir_in):
-        # file is Subject_sesXX.nii.gz
-        file_splitted = file.split("_")  # [Subject, sesXX.nii.gz]
-        subject = file_splitted[0]
-        session = file_splitted[-1].split(".")[0]
+            session_subject_path = os.path.join(subject_path, session)
 
-        print(f"Processing {file}")
+            recons_rhesus_folder = os.path.join(session_subject_path, "recons_rhesus/recon_template_space")
 
-        file_seg_in = os.path.join(seg_dir_in, file)
-        volume_in = os.path.join(volume_dir, subject, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
+            t2_subj = os.path.join(recons_rhesus_folder, "srr_template.nii.gz")
 
-        file_seg_out = os.path.join(seg_dir_out, f"{subject}_{session}_hemi.nii.gz")
-
-        try:
-            fixed = ants.image_read(volume_in)
-            fixed_seg = ants.image_read(file_seg_in)
-
-            # Find the closest atlas
-            wraped_mi_85 = ants_register(fixed, "85")
-            print(f"\tMI for 85: {wraped_mi_85}")
-            wraped_mi_110 = ants_register(fixed, "110")
-            print(f"\tMI for 85: {wraped_mi_85}")
-            wraped_mi_135 = ants_register(fixed, "135")
-            print(f"\tMI for 85: {wraped_mi_85}")
-
-            diff_wraped_mi_1 = wraped_mi_110 - wraped_mi_85
-            diff_wraped_mi_2 = wraped_mi_135 - wraped_mi_110
-
-            print(f"\tDiff 110 - 85: {diff_wraped_mi_1}")
-            print(f"\tDiff 135 - 110: {diff_wraped_mi_2}")
-
-            best_atlas = max(
-                [("85", wraped_mi_85), ("110", wraped_mi_110), ("135", wraped_mi_135)],
-                key=lambda x: x[1]
-            )[0]  # get the timepoint where the mi value is the closest to 0
-            print(f"\tClosest timepoint is : {best_atlas}")
-
-            print("\tCompute non-linear registration")
-            best_atlas_file = os.path.join(cfg.FETAL_RESUS_ATLAS, f"Template_G{best_atlas}_T2W.nii.gz")
-
-            moving_best_atlas = ants.image_read(best_atlas_file)
-            moving_seg_file = os.path.join(seg_dir_in, "dilatation/"
-            moving_best_seg = ants.image_read(moving_seg_file)
+            exit()
 
 
 
-        except Exception as e:
-            print(e)
-
-        exit()
 
