718189e82ee39cf04ad7d4a5ef256fcf7a819f87
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-11 15:06:58 +0200

    for meso

--- extraction_module/02_dilate_mask.py
+++ extraction_module/02_dilate_mask.py
@@ -4,28 +4,6 @@ import sys
 sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 
-"""
-#!/bin/bash
-
-path_in="/envau/work/meca/users/letroter.a/MarsFet/datasets/STA_atlas_processed"
-path_out="/envau/work/meca/users/letroter.a/MarsFet/datasets/STA_atlas_processed/test"
-
-for x in {21..38}
-do
-echo $x
-
-printf -v name "%02g" $x ;
-
-brainmask="${path_in}/STA${name}_all_reg_LR_dilM.nii.gz" 
-brainmask_dil="${path_out}/STA${name}_all_reg_LR_dilall.nii.gz"
-
-fslmaths $brainmask -dilM -dilM -dilM -dilM -dilall $brainmask_dil;
-fslmaths $brainmask_dil -uthr 1 $brainmask_dil;
-fslmaths $brainmask_dil -dilM $brainmask_dil;
-fslmaths $brainmask_dil -ero $brainmask_dil;
-
-done
-"""
 
 if __name__ == "__main__":
     atlas_path = os.path.join(cfg.BASE_NIOLON_PATH, "atlas_fetal_rhesus_v2")
