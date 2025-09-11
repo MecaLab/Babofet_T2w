1fd20b4ffeb4201cbfea0b8e4dc81ed41e63d8cc
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-11 09:56:38 +0200

    for meso

--- extraction_module/02_dilate_mask.py
+++ extraction_module/02_dilate_mask.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     command = f"fslmaths {filename_in} -dilM -dilM -dilM -dilM -dilall {filename_out}"
     subprocess.run(command, shell=True)
 
-    command = f"fslmaths {filename_out} -uthr 1 {filename_out}"
+    command = f"fslmaths {filename_out} -uthr 3.5 {filename_out}"
     subprocess.run(command, shell=True, check=True)
 
     command = f"fslmaths {filename_out} -dilM {filename_out}"
