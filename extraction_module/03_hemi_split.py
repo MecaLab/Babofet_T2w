ce08f824ba2e61ab22bdab230eb19243f7df075a
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-11 14:10:55 +0200

    for meso

--- extraction_module/03_hemi_split.py
+++ extraction_module/03_hemi_split.py
@@ -37,6 +37,9 @@ def find_best_atlas(fixed, atlas_path, atlas_list):
             diff_mi = current_mi - previous_mi
             print(f"\t{atlas} - {atlas_list[i-1]} = {diff_mi}")
 
+            if diff_mi > 0:
+                continue 
+
         if best_atlas is None or current_mi < best_atlas[1]:
             print("\tCurrent best atlas is", atlas)
             best_atlas = [atlas, current_mi]
