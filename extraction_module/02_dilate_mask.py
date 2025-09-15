ff59630fb1b4c209c5f575ae3a1d4b586ce172d3
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-15 13:28:43 +0200

    for niolon

--- extraction_module/02_dilate_mask.py
+++ extraction_module/02_dilate_mask.py
@@ -29,7 +29,7 @@ def dilate_bm_bis(input_file, output_file):
     command = f"fslmaths {input_file} -dilM -dilM -dilM -dilM {output_file}"
     subprocess.run(command, shell=True)
 
-    command = f"fslmaths {output_file} -uthr 2 {output_file}"
+    command = f"fslmaths {output_file} -uthr 3 {output_file}"
     subprocess.run(command, shell=True)
 
     command = f"fslmaths {output_file} -dilM {output_file}"
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         if not os.path.exists(filename_in):
             print(f"Error ! File {filename_in} does not exists. Run the previous script")
             continue
-        filename_out = os.path.join(seg_folder, "tmp", f"ONPRC_G{ts}_NFseg_3_uthr2.nii.gz")
+        filename_out = os.path.join(seg_folder, "tmp", f"ONPRC_G{ts}_NFseg_3_uthr3.nii.gz")
 
         # dilate_bm(filename_in, filename_out)
         dilate_bm_bis(filename_in, filename_out)
