56a9d57be1c0d4c3b23a0cd1f09ef72edb5dfd01
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-23 14:10:46 +0200

    for niolon

--- extraction_module/05_analyse_params.py
+++ extraction_module/05_analyse_params.py
@@ -26,5 +26,5 @@ if __name__ == "__main__":
         print(f"Gestational Day {gd}: Mean of Means = {mean_of_means:.5f}, Std of Means = {std_of_means:.5f}")
 
         best_row = df_gd.loc[df_gd["mean_distance"].idxmin()]
-        print(f"Best Parameters: Transform = {best_row['type_of_transform']},"
+        print(f"\tBest Parameters: Transform = {best_row['type_of_transform']},"
               f"Sampling Rate = {best_row['aff_random_sampling_rate']}, Mean Distance = {best_row['mean_distance']:.5f}\n")
