e023a485be6ec89fb107355caf778f62cbba2609
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-23 14:14:56 +0200

    for niolon

--- extraction_module/05_analyse_params.py
+++ extraction_module/05_analyse_params.py
@@ -26,6 +26,26 @@ if __name__ == "__main__":
         print(f"Gestational Day {gd}: Mean of Means = {mean_of_means:.5f}, Std of Means = {std_of_means:.5f}")
 
         best_row = df_gd.loc[df_gd["mean_distance"].idxmin()]
-        print(f"\tBest Parameters: Transform = {best_row['type_of_transform']},"
-              f"Sampling Rate = {best_row['aff_random_sampling_rate']}, Mean Distance = {best_row['mean_distance']:.5f}"
+        print(f"\tBest Parameters: Transform = {best_row['type_of_transform']}, "
+              f"Sampling Rate = {best_row['aff_random_sampling_rate']}, Mean Distance = {best_row['mean_distance']:.5f}, "
               f"Var Distance = {best_row['var_distance']:.5f}, Std Distance = {best_row['std_distance']:.5f}\n")
+
+    best_min_std = df.loc[df["std_distance"].idxmin()]
+    print(f"Overall Best Parameters (Min Std Dev): Gestational Day = {best_min_std['gestational_day']}, "
+          f"Transform = {best_min_std['type_of_transform']}, Sampling Rate = {best_min_std['aff_random_sampling_rate']}, "
+          f"Mean Distance = {best_min_std['mean_distance']:.5f}, Var Distance = {best_min_std['var_distance']:.5f}, "
+          f"Std Distance = {best_min_std['std_distance']:.5f}\n")
+
+    best_min_mean = df.loc[df["mean_distance"].idxmin()]
+    print(f"Overall Best Parameters (Min Mean Distance): Gestational Day = {best_min_mean['gestational_day']}, "
+          f"Transform = {best_min_mean['type_of_transform']}, Sampling Rate = {best_min_mean['aff_random_sampling_rate']}, "
+          f"Mean Distance = {best_min_mean['mean_distance']:.5f}, Var Distance = {best_min_mean['var_distance']:.5f}, "
+          f"Std Distance = {best_min_mean['std_distance']:.5f}\n")
+
+    best_min_var = df.loc[df["var_distance"].idxmin()]
+    print(f"Overall Best Parameters (Min Variance): Gestational Day = {best_min_var['gestational_day']}, "
+          f"Transform = {best_min_var['type_of_transform']}, Sampling Rate = {best_min_var['aff_random_sampling_rate']}, "
+          f"Mean Distance = {best_min_var['mean_distance']:.5f}, Var Distance = {best_min_var['var_distance']:.5f}, "
+          f"Std Distance = {best_min_var['std_distance']:.5f}\n")
+
+    
