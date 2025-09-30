4b6915be65d028fc4067503360fd3eb3e07840e4
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-30 11:30:31 +0200

    for niolon

--- extraction_module/tmp.py
+++ extraction_module/tmp.py
@@ -8,7 +8,7 @@ import configuration as cfg
 # Fonction pour calculer et afficher la similarité
 def calculate_and_plot(fixed_img, warped_path):
     warped_img = ants.image_read(warped_path)
-    similarity = ants.similarity(fixed_img, warped_img, metric='MattesMutualInformation')[0]
+    similarity = ants.similarity(fixed_img, warped_img, metric='MattesMutualInformation')
     print(f"Similarité (Mattes Mutual Information) : {similarity}")
     fixed_img.plot(title="Image Fixed")
     warped_img.plot(title="Image Warped")
@@ -21,28 +21,11 @@ if __name__ == "__main__":
 
     df = pd.read_csv(csv_path)
 
-    # Grouper par gestational_day et trouver la ligne avec la meilleure distance (max)
-    best_per_day = df.loc[df.groupby('gestational_day')['mean_distance'].idxmax()]
+    gds = [85, 155]
+    for gd in gds:
+        df_gd = df[df["gestational_day"] == gd]
+        best_data = df_gd.loc[df_gd["mean_distance"].idxmax()]
 
-    # Trouver l'atlas le plus jeune et le plus vieux avec la meilleure distance
-    youngest_best = best_per_day.loc[best_per_day['gestational_day'].str.extract('(\\d+)').astype(int).idxmin()]
-    oldest_best = best_per_day.loc[best_per_day['gestational_day'].str.extract('(\\d+)').astype(int).idxmax()]
-
-    print(youngest_best["warped_image_path"][0])
-    exit()
+        print(best_data)
 
 
-    # Charger l'image fixed (remplace par ton chemin)
-    fixed_path = os.path.join(base_path, "recons_folder/Borgne/ses07/recons_rhesus/recon_template_space",
-                              "srr_template_debiased.nii.gz")  # subject
-    fixed_img = ants.image_read(fixed_path)
-
-    # Pour l'atlas le plus jeune
-    print("Atlas le plus jeune avec la meilleure distance :")
-    print(youngest_best[['gestational_day', 'mean_distance', 'warped_image_path']])
-    calculate_and_plot(fixed_img, youngest_best['warped_image_path'])
-
-    # Pour l'atlas le plus vieux
-    print("\nAtlas le plus vieux avec la meilleure distance :")
-    print(oldest_best[['gestational_day', 'mean_distance', 'warped_image_path']])
-    calculate_and_plot(fixed_img, oldest_best['warped_image_path'])
