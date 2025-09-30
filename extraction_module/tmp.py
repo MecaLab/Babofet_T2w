3d6b00115f3e2f547fc8d0760123adada87d1b14
leroux.b <baptiste.leroux@univ-amu.fr>
2025-09-30 13:24:21 +0200

    for niolon

--- extraction_module/tmp.py
+++ extraction_module/tmp.py
@@ -12,14 +12,8 @@ def calculate_and_plot(fixed_img, warped_path):
 
     fixed_img.plot(overlay=warped_img, title=f"Best registration. Distance: {similarity}", overlay_alpha=0.5)
 
-if __name__ == "__main__":
-    base_path = cfg.BASE_NIOLON_PATH
-    atlas_path = os.path.join(base_path, "atlas_fetal_rhesus_v2")
-    registration_exp_files = os.path.join(atlas_path, "Volumes/Test_registration_borgne07_only_affine")
-    csv_path = os.path.join(registration_exp_files, "registration_results_repeated.csv")
 
-    df = pd.read_csv(csv_path)
-
+def tmp_func(df):
     df['day'] = df['gestational_day'].str.extract('(\d+)').astype(int)
 
     # 3. Grouper par gestational_day et trouver la ligne avec la meilleure distance (max)
@@ -29,7 +23,8 @@ if __name__ == "__main__":
     youngest_best = best_per_day.loc[best_per_day['day'].idxmin()]
     oldest_best = best_per_day.loc[best_per_day['day'].idxmax()]
 
-    fixed_path = os.path.join(base_path, "recons_folder/Borgne/ses07/recons_rhesus/recon_template_space", "srr_template_debiased.nii.gz") # subject
+    fixed_path = os.path.join(base_path, "recons_folder/Borgne/ses07/recons_rhesus/recon_template_space",
+                              "srr_template_debiased.nii.gz")  # subject
     fixed_img = ants.image_read(fixed_path)
 
     print("Atlas le plus jeune avec la meilleure distance :")
@@ -42,4 +37,22 @@ if __name__ == "__main__":
     print(oldest_best[['gestational_day', 'mean_distance', 'warped_image_path']])
     filename = os.path.basename(oldest_best["warped_image_path"])
     calculate_and_plot(fixed_img, os.path.join(registration_exp_files, filename))
+
+
+
+if __name__ == "__main__":
+    base_path = cfg.BASE_NIOLON_PATH
+    atlas_path = os.path.join(base_path, "atlas_fetal_rhesus_v2")
+    registration_exp_files = os.path.join(atlas_path, "Volumes/Test_registration_borgne07_only_affine")
+    csv_path = os.path.join(registration_exp_files, "registration_results_repeated.csv")
+
+    df = pd.read_csv(csv_path)
+
+    fixed_path = os.path.join(base_path, "recons_folder/Borgne/ses07/recons_rhesus/recon_template_space",
+                              "srr_template_debiased.nii.gz")  # subject
+    fixed_img = ants.image_read(fixed_path)
+
+    similarity = ants.image_similarity(fixed_img, fixed_img, metric_type='Correlation')
+
+    print(similarity)
 
