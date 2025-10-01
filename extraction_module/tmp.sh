592ed4e16a859896c28c30bad1ef64b21e9fa20c
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-01 15:32:54 +0200

    for niolon

--- extraction_module/tmp.sh
+++ extraction_module/tmp.sh
@@ -1,50 +1,37 @@
+#!/bin/bash
 module load all
 module load FSL ANTS
 
-REFERENCE='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/ses07/recons_rhesus/recon_template_space/srr_template_debiased.nii.gz'
-REFERENCE_MASK='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/ses07/recons_rhesus/recon_template_space/srr_template_mask.nii.gz'
 NEW_REF='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/ses07/recons_rhesus/recon_template_space/tmp_srr_template_debiased.nii.gz'
 
-MOVING='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Volumes/ONPRC_G122_Norm.nii.gz'
-MOVING_MASK='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Segmentations/structures_dilated/ONPRC_G122_NFseg_bm.nii.gz'
-SMOOTHED_PARCELLATIONS='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Segmentations/ONPRC_G122_NFseg.nii.gz'
-
 OUTPUT_DIR='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Volumes/flirt'
-mkdir -p "$OUTPUT_DIR"
-
-fslmaths "$REFERENCE" -mul "$REFERENCE_MASK" "$NEW_REF"
-
-echo "Measure image similarity Mattes for registration 122"
-MeasureImageSimilarity \
-  -d 3 \
-  -m Mattes["$NEW_REF", "$OUTPUT_DIR/affine_122.nii.gz", 1, 64]
-
-echo "Measure image similarity Mattes for registration 85"
-MeasureImageSimilarity \
-  -d 3 \
-  -m Mattes["$NEW_REF", "$OUTPUT_DIR/affine_85.nii.gz", 1, 64]
-
-echo "***************************************************"
-
-echo "Measure image similarity MSE for registration 122"
-MeasureImageSimilarity \
-  -d 3 \
-  -m MeanSquares["$NEW_REF", "$OUTPUT_DIR/affine_122.nii.gz", 1, 3]
-
-echo "Measure image similarity MSE for registration 85"
-MeasureImageSimilarity \
-  -d 3 \
-  -m MeanSquares["$NEW_REF", "$OUTPUT_DIR/affine_85.nii.gz", 1, 3]
+RESULTS_FILE="$OUTPUT_DIR/metrics_results.txt"
 
 
-echo "***************************************************"
+# Créer ou vider le fichier de résultats
+echo "" > "$RESULTS_FILE"
 
-echo "Measure image similarity CC for registration 122"
-MeasureImageSimilarity \
-  -d 3 \
-  -m CC["$NEW_REF", "$OUTPUT_DIR/affine_122.nii.gz", 1, 3]
+# Boucle sur tous les fichiers _affine.nii.gz dans OUTPUT_DIR
+for AFFINE_FILE in "$OUTPUT_DIR"/*_affine.nii.gz; do
+    fname=$(basename "$AFFINE_FILE" _affine.nii.gz)
+    echo "Calculating metrics for: $fname" | tee -a "$RESULTS_FILE"
 
-echo "Measure image similarity CC for registration 85"
-MeasureImageSimilarity \
-  -d 3 \
-  -m CC["$NEW_REF", "$OUTPUT_DIR/affine_85.nii.gz", 1, 3]
+    # Extraire l'âge ou l'identifiant de l'atlas (ex. G85, G122)
+    atlas_id=$(echo "$fname" | grep -oP 'G\d+')
+
+    # Mattes Mutual Information
+    echo "Mattes for $atlas_id:" | tee -a "$RESULTS_FILE"
+    MeasureImageSimilarity -d 3 -m Mattes["$NEW_REF", "$AFFINE_FILE", 1, 64] | tee -a "$RESULTS_FILE"
+
+    # Mean Squares Error
+    echo "MSE for $atlas_id:" | tee -a "$RESULTS_FILE"
+    MeasureImageSimilarity -d 3 -m MeanSquares["$NEW_REF", "$AFFINE_FILE", 1, 3] | tee -a "$RESULTS_FILE"
+
+    # Correlation Coefficient
+    echo "CC for $atlas_id:" | tee -a "$RESULTS_FILE"
+    MeasureImageSimilarity -d 3 -m CC["$NEW_REF", "$AFFINE_FILE", 1, 3] | tee -a "$RESULTS_FILE"
+
+    echo "***************************************************" | tee -a "$RESULTS_FILE"
+done
+
+echo "All metrics saved to $RESULTS_FILE"
