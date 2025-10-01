a491eb2da8680fe34e9210881c31062343d420c5
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-01 11:04:38 +0200

    for niolon

--- extraction_module/registration.sh
+++ extraction_module/registration.sh
@@ -3,6 +3,7 @@ module load FSL ANTS
 
 REFERENCE='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/ses07/recons_rhesus/recon_template_space/srr_template_debiased.nii.gz'
 REFERENCE_MASK='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/ses07/recons_rhesus/recon_template_space/srr_template_mask.nii.gz'
+NEW_REF='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/ses07/recons_rhesus/recon_template_space/tmp_srr_template_debiased.nii.gz'
 
 MOVING='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Volumes/ONPRC_G122_Norm.nii.gz'
 MOVING_MASK='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Segmentations/structures_dilated/ONPRC_G122_NFseg_bm.nii.gz'
@@ -12,11 +13,13 @@ SMOOTHED_PARCELLATIONS='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fe
 OUTPUT_DIR='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Volumes/flirt'
 mkdir -p "$OUTPUT_DIR"
 
+fslmaths "$REFERENCE" -mul "$REFERENCE_MASK" "$NEW_REF"
+
 # affine first
 echo "Running FLIRT..."
 flirt \
 	-in "$MOVING" \
-	-ref "$REFERENCE" \
+	-ref "$NEW_REF" \
 	-out "$OUTPUT_DIR/affine.nii.gz" \
 	-omat "$OUTPUT_DIR/affine.mat" \
 	-dof 12 \
