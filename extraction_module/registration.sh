18e744e3564f7d58a87cca085f9940ccdfcf69fc
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-01 15:21:59 +0200

    for niolon

--- extraction_module/registration.sh
+++ extraction_module/registration.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 module load all
 module load FSL ANTS  
 
@@ -5,26 +6,30 @@ REFERENCE='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/
 REFERENCE_MASK='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/ses07/recons_rhesus/recon_template_space/srr_template_mask.nii.gz'
 NEW_REF='/envau/work/meca/data/babofet_DB/2024_new_stuff/recons_folder/Borgne/ses07/recons_rhesus/recon_template_space/tmp_srr_template_debiased.nii.gz'
 
-MOVING='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Volumes/ONPRC_G122_Norm.nii.gz'
-MOVING_MASK='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Segmentations/structures_dilated/ONPRC_G122_NFseg_bm.nii.gz'
-SMOOTHED_PARCELLATIONS='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Segmentations/ONPRC_G122_NFseg.nii.gz'
-
-
+ATLAS_DIR='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Volumes'
 OUTPUT_DIR='/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/Volumes/flirt'
 mkdir -p "$OUTPUT_DIR"
 
+# Appliquer le masque sur la référence
 fslmaths "$REFERENCE" -mul "$REFERENCE_MASK" "$NEW_REF"
 
-# affine first
-echo "Running FLIRT..."
-flirt \
-	-in "$MOVING" \
-	-ref "$NEW_REF" \
-	-out "$OUTPUT_DIR/affine.nii.gz" \
-	-omat "$OUTPUT_DIR/affine.mat" \
-	-dof 12 \
-	-cost mutualinfo \
-	-searchrx -180 180 \
-	-searchry -180 180 \
-	-searchrz -180 180 \
-	-interp spline
+echo "=== Running FLIRT on all atlases in $ATLAS_DIR ==="
+
+for MOVING in "$ATLAS_DIR"/*.nii.gz; do
+    fname=$(basename "$MOVING" .nii.gz)
+    echo "Processing atlas: $fname"
+
+    flirt \
+        -in "$MOVING" \
+        -ref "$NEW_REF" \
+        -out "$OUTPUT_DIR/${fname}_affine.nii.gz" \
+        -omat "$OUTPUT_DIR/${fname}_affine.mat" \
+        -dof 12 \
+        -cost mutualinfo \
+        -searchrx -180 180 \
+        -searchry -180 180 \
+        -searchrz -180 180 \
+        -interp spline
+done
+
+echo "=== Finished FLIRT for all atlases ==="
