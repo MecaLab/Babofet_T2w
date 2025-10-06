26a388921470c58df9c245ef229d97663aa06a12
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-06 10:36:32 +0200

    for niolon

--- extraction_module/03_extract_surf.py
+++ extraction_module/03_extract_surf.py
@@ -28,14 +28,14 @@ if __name__ == "__main__":
         print(f"Processing subject: {subject}")
 
         for session in os.listdir(subject_src_path):
-            session_file = os.path.join(subject_src_path, session, f"{subject}_{session}_hemi.nii.gz")
+            session_file = f"{subject}_{session}_hemi.nii.gz"
             print(f"\tSession file: {session_file}")
 
             for label_name, label_val in labels_map.items():
                 print(f"\t\tProcessing {label_name}")
                 output_file = session_file.replace("_hemi.nii.gz", f".{label_name}.white.gii")
 
-                input_full_path = f"/home/atlas_fetal_rhesus_v2/Seg_Hemi/{subject}/{session_file}"
+                input_full_path = f"/home/atlas_fetal_rhesus_v2/Seg_Hemi/{subject}/{session}/{session_file}"
                 output_full_path = f"/home/atlas_fetal_rhesus_v2/Surf_Hemi/{subject}/{output_file}"
 
                 subprocess.run([
