9b2d230e2167b7e37adbcee8754d107a05c3fdb3
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-06 10:21:11 +0200

    for niolon

--- extraction_module/02_hemi_split.py
+++ extraction_module/02_hemi_split.py
@@ -223,13 +223,17 @@ if __name__ == "__main__":
 
             convert_fsl2ants(subject_output_split_seg_session, best_atlas_path, recons_rhesus_folder)
 
+            exit()
+
             mask_best_atlas = os.path.join(segmentation_atlas_path, best_atlas.replace("Norm_affine", "NFseg_bm"))
 
             filename = f"{subject}_{session}_warped_IMAGE.nii.gz"
             # ants_nonlinear_registration(subject_output_split_seg_session, recons_rhesus_folder, best_atlas_path, mask_best_atlas, filename)
 
-            subj_seg = os.path.join(segmentation_atlas_path, best_atlas.replace("Norm", "structures_dilated"))
+            subj_seg = os.path.join(segmentation_atlas_path, best_atlas.replace("Norm_affine", "structures_dilated"))
             apply_ants_transformations(subject_output_split_seg_session, recons_rhesus_folder, subj_seg)
+
+            exit()
 
             """
             warped_best_seg = ants.image_read(os.path.join(subject_output_split_seg_session, f"ants_{filename}"))
