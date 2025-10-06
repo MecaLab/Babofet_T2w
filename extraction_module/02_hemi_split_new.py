f4eb34a182d072fd659f3455a945f4a969301017
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-06 09:32:59 +0200

    for niolon

--- extraction_module/02_hemi_split_new.py
+++ extraction_module/02_hemi_split_new.py
@@ -148,6 +148,7 @@ def ants_nonlinear_registration(input_atlas_registered, base_subj_path, best_atl
             check=True,
         )"""
 
+    print(full_ouput_name)
     if os.path.exists(full_ouput_name):
         full_ouput_name_tmp = full_ouput_name.replace(" ", "")
         os.rename(full_ouput_name, full_ouput_name_tmp)
