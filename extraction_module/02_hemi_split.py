93e72b7d8316797ae125b422a369d7484e49c334
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-14 09:49:47 +0200

    for meso

--- extraction_module/02_hemi_split.py
+++ extraction_module/02_hemi_split.py
@@ -201,7 +201,7 @@ if __name__ == "__main__":
 
         subject_path = os.path.join(recons_folder, subject)
         for session in os.listdir(subject_path):
-            if session not in ["ses01", "ses04", "ses05"]:
+            if session not in ["ses04"]:
                 continue
             print(f"\tSession: {session}")
 
