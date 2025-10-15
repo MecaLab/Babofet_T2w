8c0a4a5715117c88351ad8c1ed3975ca63aa6165
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-15 14:08:20 +0200

    for meso

--- extraction_module/02_hemi_split.py
+++ extraction_module/02_hemi_split.py
@@ -240,14 +240,13 @@ if __name__ == "__main__":
             unique_label_t2 = np.unique(fixed_seg.numpy())
             if 4 in unique_label_t2:
                 seg_array = fixed_seg.numpy()
-                seg_array[seg_array == 4] = 1
+                seg_array[seg_array == 4] = 2
 
                 fixed_seg = ants.from_numpy(seg_array, origin=fixed_seg.origin, spacing=fixed_seg.spacing,
                                                 direction=fixed_seg.direction)
 
-                ants.image_write(fixed_seg, os.path.join(cfg.BASE_NIOLON_PATH, "segmentations_nnunet_mattia", f"{subject}_{session}_corrected_3.nii.gz"))
-
-            exit()
+                # ants.image_write(fixed_seg, os.path.join(cfg.BASE_NIOLON_PATH, "segmentations_nnunet_mattia", f"{subject}_{session}_corrected_3.nii.gz"))
+                
             new_data = np.zeros_like(warped_best_seg.numpy(), dtype=np.uint8)
 
             new_data[(warped_best_seg.numpy() == 1) & (fixed_seg.numpy() == 1)] = 1  # CSF droit
