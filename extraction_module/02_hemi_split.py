de1090b063571079d7f6ecc6eacd378107637616
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-15 14:00:55 +0200

    for meso

--- extraction_module/02_hemi_split.py
+++ extraction_module/02_hemi_split.py
@@ -237,6 +237,9 @@ if __name__ == "__main__":
             t2_subj_seg = os.path.join(cfg.BASE_NIOLON_PATH, "segmentations_nnunet_mattia", f"{subject}_{session}_corrected.nii.gz")   # CHANGE FOLDER NAME LATER !!!
             fixed_seg = ants.image_read(t2_subj_seg)
 
+            print(np.unique(fixed_seg))
+            exit()
+
             new_data = np.zeros_like(warped_best_seg.numpy(), dtype=np.uint8)
 
             new_data[(warped_best_seg.numpy() == 1) & (fixed_seg.numpy() == 1)] = 1  # CSF droit
