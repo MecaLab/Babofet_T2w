f6a15095802626743153f7845d629c07fbd4341d
leroux.b <baptiste.leroux@univ-amu.fr>
2025-10-16 10:11:56 +0200

    for meso

--- tools/denoising_2D.py
+++ tools/denoising_2D.py
@@ -6,6 +6,15 @@ import subprocess
 from tools import data_organization as tdo
 
 
+def nii2niigz(input_path):
+    print("Starting compression of nii files in {}".format(input_path))
+    for filename in os.listdir(input_path):
+        if filename.endswith(".nii"):
+            full_path = os.path.join(input_path, filename)
+            subprocess.run(['gzip', '-v', full_path], check=True)
+
+
+
 def denoising_data(input_path, output_path):
     DENOISING_PATH_EXE = "/scratch/lbaptiste/softs/DenoiseImage"
     base_path = input_path
@@ -23,17 +32,12 @@ def denoising_data(input_path, output_path):
 
         dir_list = os.listdir(os.path.join(base_path, subject, "scans"))
         haste_files = list()
-        truefisp_files = list()
 
         for d in dir_list:
-            d_lower = d.lower()
-            if "haste" in d_lower:
+            if "haste" in d.lower():
                 haste_files.append(d)
-            if "trufi" in d_lower:
-                truefisp_files.append(d)
 
         if len(haste_files) > 0:
-            print("\tStarting HASTE {}".format(subject))
             haste_subj_output_dir = os.path.join(subj_output_dir, "haste")
             bm_haste_subj_output_dir = os.path.join(subj_output_dir, "denoising")
 
@@ -63,37 +67,6 @@ def denoising_data(input_path, output_path):
 
                 print("\t\t\tEnd {}".format(nifti_filename))
 
-            print("\tEnding HASTE {}".format(subject))
-
-        if len(truefisp_files) > 0:
-            print("\tStarting TRUEFISP {}".format(subject))
-            truefisp_subj_output_dir = os.path.join(subj_output_dir, "truefisp")
-            bm_truefisp_subj_output_dir = os.path.join(subj_output_dir, "denoising")
-
-            if not os.path.exists(truefisp_subj_output_dir):
-                os.mkdir(truefisp_subj_output_dir)
-            if not os.path.exists(bm_truefisp_subj_output_dir):
-                os.mkdir(bm_truefisp_subj_output_dir)
-
-            for f in truefisp_files:
-                if "missfront" in f:
-                    continue
-                elif "ND" in f:
-                    continue
-                nifti_filename, nifti_full_path = tdo.file_name_from_path(base_path, subject, f)
-                s_nifti_filename = nifti_filename.split(".")
-                bm_nifti_filename = s_nifti_filename[0] + "_denoised.nii"
-                bm_full_path = os.path.join(bm_truefisp_subj_output_dir, bm_nifti_filename)
-
-                input_full_path = os.path.join(nifti_full_path, nifti_filename)
-
-                if os.path.exists(bm_full_path):
-                    print("\t\tSkipped {}".format(nifti_filename))
-                else:
-                    cmd = [DENOISING_PATH_EXE, "-i", input_full_path, "-o", bm_full_path]
-                    subprocess.run(cmd)
-
-                    print("\t\tEnd {}".format(nifti_filename))
-            print("\tEnding TRUEFISP {}".format(subject))
+            nii2niigz(bm_haste_subj_output_dir)
 
         print("Ending {}\n".format(subject))
