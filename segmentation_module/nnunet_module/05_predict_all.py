5b247296587d17bfc678e0e95c18bd5f1354be7f
leroux.b <baptiste.leroux@univ-amu.fr>
2025-11-19 16:54:10 +0100

    for meso

--- segmentation_module/nnunet_module/05_predict_all.py
+++ segmentation_module/nnunet_module/05_predict_all.py
@@ -11,7 +11,7 @@ def write_slurm_file(input_folder, output_folder, filename, partition, dataset_i
 #SBATCH --account='b219'
 #SBATCH --partition={partition}
 #SBATCH --gres=gpu:1
-#SBATCH --time=10:00
+#SBATCH --time=30:00
 #SBATCH -c 1
 #SBATCH -o predict_nnunet_%j.out
 #SBATCH -e predict_nnunet_%j.err
