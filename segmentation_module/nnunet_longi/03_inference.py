64352909c78d2185b0cdf01c0acd9884f278078d
leroux.b <baptiste.leroux@univ-amu.fr>
2025-12-02 12:44:22 +0100

    for meso

--- segmentation_module/nnunet_longi/03_inference.py
+++ segmentation_module/nnunet_longi/03_inference.py
@@ -9,7 +9,7 @@ def write_slurm_file(input_folder, output_folder, filename, dataset_id, trainer)
     slurm_content = f"""#!/bin/bash
 
 #SBATCH --account='b391'
-#SBATCH --partition=kepler
+#SBATCH --partition=pascal
 #SBATCH --gres=gpu:1
 #SBATCH --time=30:00
 #SBATCH -c 1
