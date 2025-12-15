1448a28b9abf358f8939b506c31fcb823ec6350e
leroux.b <baptiste.leroux@univ-amu.fr>
2025-12-15 15:15:24 +0100

    for meso

--- segmentation_module/nnunet_module/04_predict_all.py
+++ segmentation_module/nnunet_module/04_predict_all.py
@@ -5,13 +5,16 @@ sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 
 
-def write_slurm_file(input_folder, output_folder, filename, partition, dataset_id, trainer):
+def write_slurm_file(input_folder, output_folder, filename, partition, dataset_id, trainer, model_path):
+    cross_val_path = os.path.join(model_path, f"crossval_results_folds_0_1_2_3_4")
+    pkl_file = os.path.join(cross_val_path, "postprocessing.pkl")
+    plans_json = os.path.join(cross_val_path, "plans.json")
     slurm_content = f"""#!/bin/bash
 
 #SBATCH --account='b219'
 #SBATCH --partition={partition}
 #SBATCH --gres=gpu:1
-#SBATCH --time=30:00
+#SBATCH --time=50:00
 #SBATCH -c 12
 #SBATCH -o predict_nnunet_%j.out
 #SBATCH -e predict_nnunet_%j.err
@@ -24,7 +27,8 @@ module load cuda/12.4
 source ~/.bashrc
 conda activate nnunet
 
-nnUNetv2_predict -i {input_folder} -o {output_folder} -d {dataset_id} -c 3d_fullres -tr {trainer} -f 0 1 2 3 4
+nnUNetv2_predict -i {input_folder} -o {output_folder} -d {dataset_id} -c 3d_fullres -tr {trainer} -f 0 1 2 3 4 -p nnUNetPlans
+nnUNetv2_apply_postprocessing -i {output_folder} -o {output_folder} -pp_pkl_file {pkl_file} -np 8 -plans_json {plans_json}
 
 """
     with open(filename, "w", encoding="utf-8") as slurm_file:
