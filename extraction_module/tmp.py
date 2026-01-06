40ba8ec73e86fb75b263200b8173f13ffda8092a
leroux.b <baptiste.leroux@univ-amu.fr>
2026-01-06 10:52:01 +0100

    for niolon

--- extraction_module/tmp.py
+++ extraction_module/tmp.py
@@ -101,7 +101,10 @@ class Visualisation:
 if __name__ == "__main__":
     main_path = "/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/"
     subjects = ["Fabienne"]
-    sessions = ["03", "07"]
+    sessions = ["02", "03", "07"]
     vis = Visualisation()
-    vis.plot_mesh(os.path.join(main_path, f"Surf_Hemi/{subjects[0]}/{subjects[0]}_ses{sessions[0]}.left.white.gii"))
-    vis.save_as_html(os.path.join(main_path, f"Surf_Hemi_html/{subjects[0]}/{subjects[0]}_ses{sessions[0]}_left"))
+    for subject in subjects:
+        for session in sessions:
+            vis.plot_mesh(os.path.join(main_path, f"Surf_Hemi/{subject}/{subject}_ses{session}.left.white.gii"))
+            vis.save_as_html(os.path.join(main_path, f"Surf_Hemi_html/{subject}/{subject}_ses{session}_left"))
+            vis.show_fig()
