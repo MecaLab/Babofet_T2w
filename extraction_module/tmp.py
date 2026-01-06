a644a41678b732ec319745ed1c1605ff8081e552
leroux.b <baptiste.leroux@univ-amu.fr>
2026-01-06 09:34:39 +0100

    for niolon

--- extraction_module/tmp.py
+++ extraction_module/tmp.py
@@ -110,5 +110,5 @@ if __name__ == "__main__":
     main_path = "/envau/work/meca/data/babofet_DB/2024_new_stuff/atlas_fetal_rhesus_v2/"
     vis = Visualisation()
     vis.plot_mesh(os.path.join(main_path, "Surf_Hemi/Fabienne/Fabienne_ses02.left.white.gii"))
-    vis.save_as_html(os.path.join(main_path, "Surf_Hemi_html/Fabienne/Fabienne_ses02_left.html"))
+    vis.save_as_html(os.path.join(main_path, "Surf_Hemi_html/Fabienne/Fabienne_ses02_left"))
     vis.show_fig()
