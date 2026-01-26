b70e5bb73047417699617eea6c7c5dc17f27cc5e
leroux.b <baptiste.leroux@univ-amu.fr>
2026-01-26 13:35:07 +0100

    for meso

--- segmentation_module/nnunet_longi/02_train.py
+++ segmentation_module/nnunet_longi/02_train.py
@@ -22,6 +22,8 @@ module load cuda/12.4
 source ~/.bashrc
 conda activate nnunet
 
+cd $HOME
+
 nnUNetv2_train {dataset_id} 3d_fullres 4 -tr {trainer} --npz --val 
 """
     # nnUNetv2_train {dataset_id} 3d_fullres $SLURM_ARRAY_TASK_ID -tr {trainer} --npz
