12adb5f68f6c5506c6f141f0201c17e44a7930b6
leroux.b <baptiste.leroux@univ-amu.fr>
2026-02-02 10:18:31 +0100

    for meso

--- segmentation_module/04_predict_cascade.py
+++ segmentation_module/04_predict_cascade.py
@@ -30,16 +30,12 @@ for t in 06 07 08 09 10
 do
     echo "--- Processing Timepoint $t ---"
 
-    # 1. Création d'un dossier temporaire propre pour ce timepoint
     TMP_IN="${{OUTPUT_DIR}}/tmp_in_$t"
     TMP_OUT="${{OUTPUT_DIR}}/tmp_out_$t"
     mkdir -p "$TMP_IN" "$TMP_OUT"
 
-    # 2. Copie du volume correspondant (ex: Borgne_ses06_0000.nii.gz)
-    # On le renomme pour que l'ID de sortie soit clair
-    cp "${{INPUT_DIR}}/Borgne_ses${{t}}_0000.nii.gz" "${{TMP_IN}}/Borgne_t${{t}}_0000.nii.gz"
-
-    # 3. Lancement de LongiSeg_predict
+    cp "${{INPUT_DIR}}/Borgne_ses${{t}}_0000.nii.gz" "${{TMP_IN}}/Borgne_ses${{t}}_0000.nii.gz"
+    
     LongiSeg_predict -i "$TMP_IN" \\
                      -o "$TMP_OUT" \\
                      -d 1 \\
@@ -48,15 +44,13 @@ do
                      --save_probabilities \\
                      -pat "$PATIENT_JSON"
 
-    # 4. Post-processing (optionnel mais recommandé si calculé au training)
     LongiSeg_apply_postprocessing -i "$TMP_OUT" \\
                                   -o "$TMP_OUT" \\
                                   -pp_pkl_file "$PKL_FILE" \\
                                   -np 8 \\
                                   -plans_json "$PLANS_JSON"
 
-    # 5. Déplacement vers le dossier final et nettoyage
-    mv "${{TMP_OUT}}/Borgne_t${{t}}.nii.gz" "${{OUTPUT_DIR}}/seg_Borgne_t${{t}}.nii.gz"
+    mv "${{TMP_OUT}}/Borgne_ses${{t}}.nii.gz" "${{OUTPUT_DIR}}/seg_Borgne_ses${{t}}.nii.gz"
 
     rm -rf "$TMP_IN" "$TMP_OUT"
 done
