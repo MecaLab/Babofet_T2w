75270dcdb514f25d5c940639bcf12a569f520e9e
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-03 10:13:39 +0100

    starting file

---
+++ bids_script/01_create_anat.py
@@ -0,0 +1,51 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
+
+
+INPUT_PATH = os.path.join(cfg.BASE_NIOLON_PATH, "subjects")
+OUTPUT_PATH = "/envau/work/meca/data/babofet_BIDS/sourcedata/raw"
+
+AX_MATCH = {
+    "AX": "01",
+    "AX2": "02",
+    "COR": "03",
+    "COR2": "04",
+    "SAG": "05",
+    "SAG2": "06"
+}
+
+subjects_data = {
+    "Borgne": ["ses01", "ses03", "ses04", "ses05", "ses06", "ses07", "ses08", "ses09", "ses10"]
+}
+
+def format_session_str(sess):
+    id_sess = sess[3:]
+    return f"ses-{id_sess}"
+
+if __name__ == "__main__":
+
+    for subject, sessions in subjects_data.items():
+        output_subj_dir = os.path.join(OUTPUT_PATH, f"sub-{subject}")
+
+        if not os.path.exists(output_subj_dir):
+            os.makedirs(output_subj_dir)
+
+        for session in sessions:
+            session_formated = format_session_str(session)
+            sub_session_dir = os.path.join(output_subj_dir, session_formated)
+
+            if not os.path.exists(sub_session_dir):
+                os.makedirs(sub_session_dir)
+
+            anat_dir = os.path.join(sub_session_dir, "anat")
+            if not os.path.exists(anat_dir):
+                os.makedirs(anat_dir)
+
+            input_subj_dir = os.path.join(INPUT_PATH, f"sub-{subject}_{session_formated}", "scans")
+
+            print(input_subj_dir)
+            exit()
+
+
