7a59fc5989f9e1eccd9c70fc834086d9e6275075
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-04 15:25:58 +0100

    reformat code for bids format

--- nifty_module/01_denoising.py
+++ nifty_module/01_denoising.py
@@ -72,7 +72,8 @@ def denoising_data_bids_format(input_path, output_path):
     DENOISING_PATH_EXE = os.path.join(cfg.SOFTS_PATH, "DenoiseImage")
 
     for folder in os.listdir(input_path):
-        print(folder, os.path.isfile(folder))
+        if folder.startswith("sub"):
+            print(folder)
 
 
 if __name__ == "__main__":
