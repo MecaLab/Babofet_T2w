467113717de1003fa34289c32c8bba110524bdd5
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-04 16:05:51 +0100

    cp fetalbet mask to bids

---
+++ bids_script/03_create_fetalBET.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import shutil
+from pathlib import Path
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
+
+
+if __name__ == "__main__":
+
+    INPUT_PATH = os.path.join(cfg.BASE_NIOLON_PATH, "fetalBET_masks_v2")
+    OUTPUT_PATH = os.path.join(cfg.DERIVATIVES_BIDS_PATH, "niftymic")
+
+    for folder in os.listdir(INPUT_PATH):
+        print(folder)
