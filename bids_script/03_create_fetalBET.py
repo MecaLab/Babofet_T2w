432577da80dcfbbfffdb0430d304b093c16e85b7
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-05 10:41:37 +0100

    fetalbet mask mv

--- bids_script/03_create_fetalBET.py
+++ bids_script/03_create_fetalBET.py
@@ -6,10 +6,26 @@ sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 
 
+
+
+
 if __name__ == "__main__":
+
+    VIEW_MATCH = {
+        "AX": "01",
+        "AX2": "02",
+        "COR": "03",
+        "COR2": "04",
+        "SAG": "05",
+        "SAG2": "06"
+    }
 
     INPUT_PATH = os.path.join(cfg.BASE_NIOLON_PATH, "fetalBET_masks_V2")
     OUTPUT_PATH = os.path.join(cfg.DERIVATIVES_BIDS_PATH, "niftymic")
 
+    # sub-<sub>_ses-<ses>_acq-haste_run-<01..06>_desc-brain_mask.nii.gz
+
     for folder in os.listdir(INPUT_PATH):
-        print(folder)
+        folder_path = os.path.join(INPUT_PATH, folder)
+        print(folder_path)
+        exit()
