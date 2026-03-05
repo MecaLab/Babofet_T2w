f2cb554aa98bf00ae1a245915e76d72bd1fac4f4
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-05 14:09:10 +0100

    fetalbet mask mv

--- bids_script/03_create_fetalBET.py
+++ bids_script/03_create_fetalBET.py
@@ -11,7 +11,7 @@ def extract_orientation(filename):
     mapping = {
         "axial": "AX",
         "coronal": "COR",
-        "sagittal": "SAG",
+        "sagital": "SAG",
         "ax": "AX",
         "cor": "COR",
         "sag": "SAG"
