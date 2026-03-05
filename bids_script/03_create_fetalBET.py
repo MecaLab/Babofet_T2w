92fa1cfd6ff6aea436c10e10a0d1c327dd7c86a6
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-05 13:24:38 +0100

    fetalbet mask mv

--- bids_script/03_create_fetalBET.py
+++ bids_script/03_create_fetalBET.py
@@ -48,4 +48,6 @@ if __name__ == "__main__":
                 sub_subject, sub_session = folder.split("_")
                 filename_output = f"{sub_subject}_{sub_session}_acq-haste_run-{VIEW_MATCH[orientation]}_desc-brain_mask.nii.gz"
                 output_file_path = os.path.join(OUTPUT_PATH, sub_subject, sub_session, "anat", filename_output)
+                if not os.path.exists(output_file_path):
+                    os.makedirs(output_file_path)
                 shutil.copy(input_file_path, output_file_path)
