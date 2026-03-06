e63f641cf983048dc4ddf15b7cb74c7f8dfcb5e1
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-06 16:39:37 +0100

    first commit for nnunet

--- bids_script/05_create_nnunet.py
+++ bids_script/05_create_nnunet.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 import pandas as pd
 sys.path.insert(0, os.path.abspath(os.curdir))
@@ -7,6 +8,10 @@ import subprocess
 import nibabel as nib
 import numpy as np
 
+
+def format_session_str(sess):
+    id_sess = sess[3:]
+    return f"ses-{id_sess}"
 
 def create_segmentation_mapping_tsv(output_filename):
     data = {
@@ -24,14 +29,29 @@ def create_segmentation_mapping_tsv(output_filename):
     print(f"Mapping file created: {output_filename}")
 
 
-
 if __name__ == "__main__":
     input_path = os.path.join(cfg.BASE_NIOLON_PATH, "pred_dataset_12")  # need to adapt this later
-    output_path = os.path.join(cfg.DERIVATIVES_BIDS_PATH, "nnunet")
+    output_base_path = os.path.join(cfg.DERIVATIVES_BIDS_PATH, "nnunet")
 
-    tsv_file = os.path.join(output_path, "dseg.tsv")
+    tsv_file = os.path.join(output_base_path, "dseg.tsv")
     if not os.path.exists(tsv_file):
         create_segmentation_mapping_tsv(tsv_file)
 
     for file in os.listdir(input_path):
-        print(file)
+        if file.endswith(".nii.gz"):
+            print(f"Processing {file}")
+            subj_sess = file.split(".")[0]
+            subj, sess = subj_sess.split("_")
+            sess_formated = format_session_str(sess)
+
+            input_full_path = os.path.join(input_path, file)
+
+            # sub-<sub>_ses-<ses>_seg-braintissues_dseg.nii.gz
+            output_file = f"sub-{subj}_{sess_formated}_seg-braintissues_dseg.nii.gz"
+            output_path = os.path.join(output_base_path, f"sub-{subj}", sess_formated, "anat")
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+
+            output_full_path = os.path.join(output_path, output_file)
+            shutil.copy(input_full_path, output_full_path)
+            exit()
