9d59fc500c78ad1470f785ca298db522ce292e82
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-11 19:06:06 +0100

    debias code

--- nifty_module/04_debias.py
+++ nifty_module/04_debias.py
@@ -6,34 +6,40 @@ import configuration as cfg
 
 
 if __name__ == "__main__":
-    bias_field_correction_path = "/hpc/shared/apps/x86_64/softs/ANTS/2.3.4/bin/N4BiasFieldCorrection"  # path on niolon
-
-    # command
     """
     N4BiasFieldCorrection -d 3 -i STACK_PATH -x MASK_PATH -o OUTPUT_PATH
     """
+    bias_field_correction_path = "/hpc/shared/apps/x86_64/softs/ANTS/2.3.4/bin/N4BiasFieldCorrection"  # path on niolon
+    base_path = cfg.DERIVATIVES_BIDS_PATH
+    subject = "sub-Aziza"
+    session = "ses-02"
 
-    subject = sys.argv[1]
-    base_path = os.path.join(cfg.RECONS_FOLDER, subject)
+    print(f"Processing {subject} {session}...")
 
-    for session in os.listdir(base_path):
-        print(f"Processing {subject} {session}...")
-        session_path = os.path.join(base_path, session, "recons_rhesus/recon_template_space")
+    subj_recons_path = os.path.join(base_path, "intermediate", "niftymic", subject, session, "reconstruction_niftymic")
+    if not os.path.exists(subj_recons_path):
+        print(f"Subject reconstruction path does not exist: {subj_recons_path}")
+        sys.exit(1)
 
-        stack_path = os.path.join(session_path, "srr_template.nii.gz")
-        mask_path = os.path.join(session_path, "srr_template_mask.nii.gz")
+    recon_template_space_dir = os.path.join(subj_recons_path, "recon_template_space")
+    if not os.path.exists(recon_template_space_dir):
+        print(f"Reconstruction template space directory does not exist: {recon_template_space_dir}")
+        sys.exit(1)
 
-        if not os.path.exists(stack_path):
-            print(f"\tStack file {stack_path} does not exist, skipping...")
-            continue
+    stack_path = os.path.join(recon_template_space_dir, "srr_template.nii.gz")
+    mask_path = os.path.join(recon_template_space_dir, "srr_template_mask.nii.gz")
 
-        output_filename = os.path.join(session_path, "srr_template_debiased.nii.gz")
-        if os.path.exists(output_filename):
-            print(f"\tOutput file {output_filename} already exists, skipping...")
-            continue
+    if not os.path.exists(stack_path):
+        print(f"\tStack file {stack_path} does not exist, skipping...")
+        exit()
 
-        subprocess.run([bias_field_correction_path, "-d", "3", "-i", stack_path, "-x", mask_path, "-o", output_filename])
-        print(f"N4BiasFieldCorrection done !")
+    output_filename = os.path.join(recon_template_space_dir, "srr_template_debiased.nii.gz")
+    if os.path.exists(output_filename):
+        print(f"\tOutput file {output_filename} already exists, skipping...")
+        exit()
+
+    subprocess.run([bias_field_correction_path, "-d", "3", "-i", stack_path, "-x", mask_path, "-o", output_filename])
+    print(f"N4BiasFieldCorrection done !")
 
 
 
