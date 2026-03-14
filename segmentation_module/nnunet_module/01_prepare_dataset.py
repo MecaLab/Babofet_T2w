02b21b14622b12a154ceff62e4701011eb960f95
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-14 13:08:55 +0100

    prepare nnunet dataset for niolon

--- segmentation_module/nnunet_module/01_prepare_dataset.py
+++ segmentation_module/nnunet_module/01_prepare_dataset.py
@@ -1,8 +1,14 @@
 import os
+import shutil
 import sys
 import json
 sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
+
+
+def format_session_str(sess):
+    id_sess = sess[3:]
+    return f"ses-{id_sess}"
 
 
 def write_json_file(path, num_training, dataset_name, use_debias=False):
@@ -33,6 +39,17 @@ def write_json_file(path, num_training, dataset_name, use_debias=False):
 
 if __name__ == "__main__":
 
+    base_path = cfg.DERIVATIVES_BIDS_PATH
+    input_base_path = os.path.join(base_path, "intermediate", "niftymic")
+
+    if not os.path.exists(input_base_path):
+        raise FileNotFoundError(f"Input base path does not exist: {input_base_path}")
+
+    output_base_path = os.path.join(base_path, "intermediate", "nnunet")
+
+    if not os.path.exists(output_base_path):
+        os.makedirs(output_base_path)
+
     config_json = "segmentation_module/nnunet_module/config.json"
     with open(config_json, "r") as file:
         data = json.load(file)
@@ -40,7 +57,7 @@ if __name__ == "__main__":
     train_subject_sessions = data["train_subject_sessions"]
     test_subject_sessions = data["test_subject_sessions"]
 
-    seg_dataset = "gt_dataset_2"
+    seg_dataset = os.path.join(cfg.BASE_NIOLON_PATH, "gt_dataset")
 
     id_dataset = int(sys.argv[1])  # should be integer, eg, 1, 2, 3, etc.
     name = sys.argv[2]  # the dataset name, can be whatever you want, but you will need to use it later so remember it
@@ -65,49 +82,56 @@ if __name__ == "__main__":
     print("Training files...")
     for subject, sessions in train_subject_sessions.items():
         print(f"\tProcessing subject: {subject}")
-        input_path_3d_segs = os.path.join(cfg.BASE_PATH, seg_dataset, "train_dataset")
-
-        input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
+        input_path_3d_segs = os.path.join(seg_dataset, "train_dataset")
+        input_path_3d_stacks = os.path.join(input_base_path, subject)
 
         for session in sessions:
-            print(f"\t\tProcessing session: {session}")
+            session_formatted = format_session_str(session)
+            print(f"\t\tProcessing session: {session_formatted}")
 
-            input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template.nii.gz")
+            input_path_3d_stack = os.path.join(input_path_3d_stacks, session_formatted, "reconstruction_niftymic",
+                                               "recon_template_space", "srr_template.nii.gz")
             input_path_3d_seg = os.path.join(input_path_3d_segs, f"{subject}_{session}.nii.gz")
 
             output_path_3d_stack = os.path.join(images_tr_path, f"{subject}_{session}_0000.nii.gz")
             output_path_3d_seg = os.path.join(labels_tr_path, f"{subject}_{session}.nii.gz")
 
-            os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
-            os.system(f"cp {input_path_3d_seg} {output_path_3d_seg}")
+            shutil.copy(input_path_3d_stack, output_path_3d_stack)
+            shutil.copy(input_path_3d_seg, output_path_3d_seg)
 
             if use_debias:
-                input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
+                input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session_formatted,
+                                                       "reconstruction_niftymic", "recon_template_space",
+                                                       "srr_template_debiased.nii.gz")
                 output_path_3d_stack_bis = os.path.join(images_tr_path, f"{subject}_{session}_0001.nii.gz")
 
-                os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
+                shutil.copy(input_path_3d_stack_bis, output_path_3d_stack_bis)
 
     print("Testing files...")
     for subject, sessions in test_subject_sessions.items():
         print(f"\tProcessing subject: {subject}")
-        input_path_3d_segs = os.path.join(cfg.BASE_PATH, seg_dataset, "test_dataset")
+        input_path_3d_segs = os.path.join(seg_dataset, "test_dataset")
 
-        input_path_3d_stacks = os.path.join(cfg.DATA_PATH, subject)
+        input_path_3d_stacks = os.path.join(input_base_path, subject)
 
         for session in sessions:
-            print(f"\t\tProcessing session: {session}")
+            session_formatted = format_session_str(session)
+            print(f"\t\tProcessing session: {session_formatted}")
 
-            input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template.nii.gz")
+            input_path_3d_stack = os.path.join(input_path_3d_stacks, session_formatted, "reconstruction_niftymic",
+                                               "recon_template_space", "srr_template.nii.gz")
 
             output_path_3d_stack = os.path.join(images_ts_path, f"{subject}_{session}_0000.nii.gz")
 
-            os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
+            shutil.copy(input_path_3d_stack, output_path_3d_stack)
 
             if use_debias:
-                input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
+                input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session_formatted,
+                                                       "reconstruction_niftymic", "recon_template_space",
+                                                       "srr_template_debiased.nii.gz")
                 output_path_3d_stack_bis = os.path.join(images_ts_path, f"{subject}_{session}_0001.nii.gz")
 
-                os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
+                shutil.copy(input_path_3d_stack_bis, output_path_3d_stack_bis)
 
     dataset_json = os.path.join(output_path, "dataset.json")
     num_training = len(os.listdir(labels_tr_path))
@@ -115,4 +139,4 @@ if __name__ == "__main__":
 
     print("Dataset preparation completed")
     print("Running dataset check...")
-    os.system(f"nnUNetv2_plan_and_preprocess -d {id_dataset} --verify_dataset_integrity")
+    # os.system(f"nnUNetv2_plan_and_preprocess -d {id_dataset} --verify_dataset_integrity")
