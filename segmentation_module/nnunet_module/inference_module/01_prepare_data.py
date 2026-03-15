470a69d0fd416e0468ce120d76493f4b8dc5bdf4
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-15 11:29:07 +0100

    implementing inference nnunet on niolon

---
+++ segmentation_module/nnunet_module/inference_module/01_prepare_data.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+import json
+import sys
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
+
+def format_session_str(sess):
+    id_sess = sess[3:]
+    return f"ses-{id_sess}"
+
+
+if __name__ == "__main__":
+    config_json = "segmentation_module/nnunet_module/config.json"
+    with open(config_json, "r") as file:
+        data = json.load(file)
+
+    inference_data = data["inference_subject_sessions"]
+
+    input_base_path = os.path.join(cfg.DERIVATIVES_BIDS_PATH, "niftymic")
+    output_base_path = os.path.join(cfg.DERIVATIVES_BIDS_PATH, "intermediate", "nnunet", "inference_dataset")
+
+    if not os.path.exists(output_base_path):
+        os.makedirs(output_base_path)
+
+    for subject, sessions in inference_data.items():
+        print(f"Processing subject: {subject}")
+        input_path_3d_stacks = os.path.join(input_base_path, subject)
+
+        for session in sessions:
+            session_formatted = format_session_str(session)
+            print(f"\tProcessing session: {session_formatted}")
+
+            input_path_3d_stack = os.path.join(input_path_3d_stacks, session_formatted, "reconstruction_niftymic",
+                                               "recon_template_space", "srr_template.nii.gz")
+            output_path_3d_stack = os.path.join(output_base_path, f"{subject}_{session}_0000.nii.gz")
+            shutil.copy(input_path_3d_stack, output_path_3d_stack)
+
+
