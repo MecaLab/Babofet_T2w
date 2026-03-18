d30caeaef7368c0168410137603de483e394c828
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-18 09:42:35 +0100

    nnunet longi train for meso

--- segmentation_module/longiseg_module/train_module/01_prepare_dataset.py
+++ segmentation_module/longiseg_module/train_module/01_prepare_dataset.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 
 
-def write_json_file(path, num_training, dataset_name, use_debias=False):
+def write_json_file(path, num_training, dataset_name):
     dico_data = {
         "channel_names": {
             "0": "T2W",
@@ -21,11 +21,6 @@ def write_json_file(path, num_training, dataset_name, use_debias=False):
         "file_ending": ".nii.gz",
         "dataset_name": dataset_name
     }
-    if use_debias:
-        dico_data["channel_names"] = {
-            "0": "T2W",
-            "1": "T2W",
-        }
 
     with open(path, 'w') as f:
         json.dump(dico_data, f, indent=4)
@@ -45,15 +40,14 @@ if __name__ == "__main__":
     train_subject_sessions = data["train_subject_sessions"]
     test_subject_sessions = data["test_subject_sessions"]
 
-    seg_dataset = "gt_dataset_2"
+    seg_dataset = os.path.join(cfg.BASE_PATH, "gt_dataset_2")
 
     id_dataset = int(sys.argv[1])  # should be integer, eg, 1, 2, 3, etc.
     name = sys.argv[2]  # the dataset name, can be whatever you want, but you will need to use it later so remember it
-    use_debias = False # sys.argv[3].lower() == 'true'  # whether to use debiasing or not
 
     dataset_name = f"Dataset{id_dataset:03d}_{name}"
 
-    output_path = os.path.join(cfg.LONGISEG_RAW_PATH, dataset_name)
+    output_path = os.path.join(cfg.LONGISEG_RAW_PATH_MESO, dataset_name)
 
     images_tr_path = os.path.join(output_path, "imagesTr")
     images_ts_path = os.path.join(output_path, "imagesTs")
@@ -91,13 +85,6 @@ if __name__ == "__main__":
             os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
             os.system(f"cp {input_path_3d_seg} {output_path_3d_seg}")
 
-
-            if use_debias:
-                input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
-                output_path_3d_stack_bis = os.path.join(images_tr_path, f"{subject}_{session}_0001.nii.gz")
-
-                os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
-
     print("Testing files...")
     for subject, sessions in test_subject_sessions.items():
         print(f"\tProcessing subject: {subject}")
@@ -108,23 +95,20 @@ if __name__ == "__main__":
         for session in sessions:
             print(f"\t\tProcessing session: {session}")
 
+            # dico_subj[subject].append(f"{subject}_{session}")
+
             input_path_3d_stack = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template.nii.gz")
 
             output_path_3d_stack = os.path.join(images_ts_path, f"{subject}_{session}_0000.nii.gz")
 
             os.system(f"cp {input_path_3d_stack} {output_path_3d_stack}")
 
-            if use_debias:
-                input_path_3d_stack_bis = os.path.join(input_path_3d_stacks, session, "recons_rhesus/recon_template_space/srr_template_debiased.nii.gz")
-                output_path_3d_stack_bis = os.path.join(images_ts_path, f"{subject}_{session}_0001.nii.gz")
-
-                os.system(f"cp {input_path_3d_stack_bis} {output_path_3d_stack_bis}")
 
     dataset_json = os.path.join(output_path, "dataset.json")
     patients_tr_json = os.path.join(output_path, "patientsTr.json")
 
     num_training = len(os.listdir(labels_tr_path))
-    write_json_file(dataset_json, num_training, dataset_name, use_debias)
+    write_json_file(dataset_json, num_training, dataset_name)
     write_patients_tr(patients_tr_json, dico_subj)
 
     print("Dataset preparation completed")
