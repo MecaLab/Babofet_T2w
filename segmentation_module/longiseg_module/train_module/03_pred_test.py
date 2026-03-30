619d4d30751fafc834435eb584afa7c42bc2cd46
leroux.b <baptiste.leroux@univ-amu.fr>
2026-03-30 09:46:39 +0200

    test pred late sess

--- segmentation_module/longiseg_module/train_module/03_pred_test.py
+++ segmentation_module/longiseg_module/train_module/03_pred_test.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import sys
+from collections import defaultdict
+import json
 sys.path.insert(0, os.path.abspath(os.curdir))
 import configuration as cfg
 
@@ -41,6 +43,34 @@ LongiSeg_predict -i {input_folder} -o {output_folder} -d {dataset_id} -c 3d_full
     os.chmod(filename, 0o700)
 
 
+def generate_patient_sessions_json(directory_path, output_filename="patientsTs.json"):
+    # defaultdict handles the creation of lists for new keys automatically
+    patient_data = defaultdict(list)
+
+    files = os.listdir(directory_path)
+
+    files.sort()
+
+    for filename in files:
+        if filename.endswith(".nii.gz"):
+            # Example: "Borgne_ses05_0000.nii.gz" -> ["Borgne", "ses05", "0000.nii.gz"]
+            parts = filename.split('_')
+
+            if len(parts) >= 2:
+                patient_name = parts[0]
+                session_id = parts[1]
+
+                session_entry = f"{patient_name}_{session_id}"
+
+                if session_entry not in patient_data[patient_name]:
+                    patient_data[patient_name].append(session_entry)
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        json.dump(dict(patient_data), f, indent=4)
+
+    print(f"File '{output_filename}' has been created successfully.")
+
+
+
 if __name__ == "__main__":
     dataset_id = int(sys.argv[1])
     name = sys.argv[2]
@@ -52,7 +82,10 @@ if __name__ == "__main__":
     model_path = os.path.join(cfg.LONGISEG_RESULTS_PATH_MESO, dataset_name, f"{trainer}__nnUNetPlans__3d_fullres")
 
     input_folder = os.path.join(cfg.LONGISEG_RAW_PATH_MESO, dataset_name, "imagesTs")
-    patients_json = os.path.join(cfg.LONGISEG_RAW_PATH_MESO, dataset_name, "patientsTr.json")
+
+    test_pred_json = os.path.join(cfg.LONGISEG_RAW_PATH_MESO, dataset_name, "patientsTs.json")
+    generate_patient_sessions_json(directory_path=input_folder, output_filename=test_pred_json)
+
     output_folder = os.path.join(cfg.CODE_PATH, f"snapshots/longiseg_res/pred_dataset_{dataset_id}")
 
     if not os.path.exists(output_folder):
@@ -61,5 +94,5 @@ if __name__ == "__main__":
     print("Starting inference")
 
     filename = "slurm_files/longiseg_prediction.slurm"
-    write_slurm_file(input_folder, output_folder, filename, dataset_id, trainer, model_path, patients_json, partition)
+    write_slurm_file(input_folder, output_folder, filename, dataset_id, trainer, model_path, test_pred_json, partition)
     subprocess.run(["sbatch", filename])
