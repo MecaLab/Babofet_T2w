f5e073aefcb42d062c261fbfa5b72d884add732d
leroux.b <baptiste.leroux@univ-amu.fr>
2026-04-01 15:18:11 +0200

    file to retrieve model zip

--- segmentation_module/inference_module/01_retrieve_model.py
+++ segmentation_module/inference_module/01_retrieve_model.py
@@ -1,7 +1,9 @@
 import subprocess
+import zipfile
 import sys
 import os
-
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
 
 def retrieve_zip(zip_server_path, dst_path):
     MESO_USER = "lbaptiste"
@@ -22,9 +24,7 @@ def retrieve_zip(zip_server_path, dst_path):
 
     try:
         print(f"Executing: {' '.join(command)}")
-        # run() waits for the command to finish
-        result = subprocess.run(command, check=True, capture_output=True, text=True)
-        print(result)
+        subprocess.run(command, check=True, capture_output=True, text=True)
         print("File retrieved successfully.")
         return True
     except subprocess.CalledProcessError as e:
@@ -32,7 +32,35 @@ def retrieve_zip(zip_server_path, dst_path):
         return False
 
 
+def unzip_file(zip_path, extract_to):
+    """
+    Unzips a ZIP archive to a specific destination.
+    """
+    if not os.path.exists(zip_path):
+        print(f"Error: The file {zip_path} does not exist.")
+        return False
+
+    # Create destination directory if it doesn't exist
+    if not os.path.exists(extract_to):
+        os.makedirs(extract_to)
+
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_to)
+            print(f"Success: Extracted to {extract_to}")
+            return True
+    except zipfile.BadZipFile:
+        print("Error: The file is corrupted or not a zip file.")
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+
+
 if __name__ == "__main__":
     zip_server_path = sys.argv[1]
     dst_path = sys.argv[2]
-    retrieve_zip(zip_server_path, dst_path)
+    # retrieve_zip(zip_server_path, dst_path)
+
+    unzip_file(dst_path, cfg.LONGISEG_RESULTS_PATH)
