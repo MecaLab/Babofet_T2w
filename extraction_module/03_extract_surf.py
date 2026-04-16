a13d33c8a83d36f526d17a4f858bb670d00ec52a
leroux.b <baptiste.leroux@univ-amu.fr>
2026-04-16 14:32:17 +0200

    surface extraction pipeline for bids

--- extraction_module/03_extract_surf.py
+++ extraction_module/03_extract_surf.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
                     output_full_path = os.path.join(subject_dst_path, output_file_pattern)
 
                     subprocess.run([
-                        "python3", "surface_processing/generate_mesh.py",
+                        "python", "surface_processing/generate_mesh.py",
                         "-s", input_full_path,
                         "-l", str(label_val),
                         "-m", output_full_path,
