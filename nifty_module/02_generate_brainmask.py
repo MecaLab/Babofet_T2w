0cd88c86d989cb964e2e0a8f0d06a824efbed3f8
leroux.b <baptiste.leroux@univ-amu.fr>
2026-04-20 13:15:09 +0200

    fetalbet mask implementation

---
+++ nifty_module/02_generate_brainmask.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import subprocess
+sys.path.insert(0, os.path.abspath(os.curdir))
+import configuration as cfg
+
+
+if __name__ == "__main__":
+
+    input_path = os.path.join(cfg.DERIVATIVES_BIDS_PATH, "intermediate", "niftymic")
+    output_path = os.path.join(cfg.DERIVATIVES_BIDS_PATH, "intermediate", "fetalbet_brainmask")
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    subjects = ['sub-Filoutte']
+    sessions = ['ses-01']
+
+
+    for subject in subjects:
+        print(f"Processing {subject}")
+        for session in sessions:
+            print(f"\tProcessing {session}")
+            subject_path = os.path.join(input_path, subject, session)
+
+            output_subject_path = os.path.join(output_path, subject_path, session)
+            if not os.path.exists(output_subject_path):
+                os.makedirs(output_subject_path)
+
+            for file in os.listdir(subject_path):
+                if file.endswith(".nii.gz") and "denoised" in file:
+                    image_path = os.path.join(subject_path, file)
+                    output_folder = os.path.join(output_subject_path, file)
+
+                    print(f"Processing {image_path}...")
+                    subprocess.run([
+                        'python', 'fetalbet_module/ensemble_inference.py',
+                        '--image_path', image_path,
+                        '--output_folder', output_folder
+                    ])
+
+
